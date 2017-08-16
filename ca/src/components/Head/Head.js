8d1c716c5a1195c97069e3c63bd56c41bdf5c038
danny <dammy021406@gmail.com[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Dnn>
2017-08-16 11:50:35 +0800

    test get json replace dcard with correct rout

--- ca/src/components/Head/Head.js
+++ ca/src/components/Head/Head.js
@@ -61,7 +61,7 @@ class Head extends Component {
 
     getdata(){
     	var _this = this;
-        return axios.get('https://www.dcard.tw/_api/posts').then(studentData => {
+        return axios.get('/students/info').then(studentData => {
             studentData.status // HTTP response code (e.g., 200, 401)
             studentData.data // object parsed from HTTP response body
             studentData.headers // HTTP presonse headers
