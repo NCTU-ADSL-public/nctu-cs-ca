475510b68c0c7ac7d51a0e1d0e4a29848fe8df88
danny <dammy021406@gmail.com[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Dnn>
2017-08-19 21:55:44 +0800

    login status test

--- ca/src/components/Login/Login.js
+++ ca/src/components/Login/Login.js
@@ -3,41 +3,104 @@ import LoginButtom from './LoginButtom.js';
 import './Login.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import FadeIn from 'react-fade-in';
+import axios from 'axios';
 
 
 
 
 class Login extends Component {
 
+    constructor(props) {
+        super(props);
+        this.getLoginstatus();
+    }
 
-  render() {
-    return (
-      <div className="Login" >   
+    getLoginstatus(){
+        var _this = this;
+        return axios.get('/home/status').then(loginStatus => {
+            loginStatus.status // HTTP response code (e.g., 200, 401)
+            loginStatus.data // object parsed from HTTP response body
+            loginStatus.headers // HTTP presonse headers
+            _this.loginStatusHandler(loginStatus.data.state);
+            console.log(loginStatus.data);
+        }).catch(err => {
+            console.log(err);
+        });
+    }
 
+    loginStatusHandler(index){
+        if(index === '0'){
+            this.setState({
+                LoginMessage_state1: {
+                    display:'none',
+                },
+                LoginMessage_state2:{
+                    display:'none',
+                }
+            })
+        }
 
-            <div id="AjustToggleButtom"></div>
-            <div className="Login-header"   ref="tip">
+        else if(index === '1'){
+            this.setState({
+                LoginMessage_state1: {
+                    display:'inline',
+                },
+                LoginMessage_state2:{
+                    display:'none',
+                }
+            })
+        }
+        else if(index === '1'){
+            this.setState({
+                LoginMessage_state1: {
+                    display:'none',
+                },
+                LoginMessage_state2:{
+                    display:'inline',
+                }
+            })
+        }
+    }
 
-            <FadeIn>
-                <div id="rectangle"></div>
-                <div id="eng-title"><div id="h11">NCTU Curriculum Assistant</div></div>
-                <div id="ch-title"><div id="h22">交大資工線上助理</div></div>
+    state = {
+        LoginMessage_state1:{
+            display:'inline',
+        },
+        LoginMessage_state2:{
+            display:'inline',
+        }
+    };
 
-                <div className = "Login-login">
-                  <MuiThemeProvider>
-                    <LoginButtom />
-                  </MuiThemeProvider>
+      render() {
+        return (
+          <div className="Login" >
+
+              <div style = {this.state.LoginMessage_state1}>Oops you are not cs student !</div>
+              <br/>
+              <div style = {this.state.LoginMessage_state2}>Please login first</div>
+                <div id="AjustToggleButtom"></div>
+                <div className="Login-header"   ref="tip">
+
+                <FadeIn>
+                    <div id="rectangle"></div>
+                    <div id="eng-title"><div id="h11">NCTU Curriculum Assistant</div></div>
+                    <div id="ch-title"><div id="h22">交大資工線上助理</div></div>
+
+                    <div className = "Login-login">
+                      <MuiThemeProvider>
+                        <LoginButtom />
+                      </MuiThemeProvider>
+                    </div>
+
+                </FadeIn>
                 </div>
 
-            </FadeIn>
-            </div>
 
 
-
-            <footer>Copyright @2017 NCTUCS 交通大學資訊工程學系</footer>
-      </div>
-    );
-  }
+                <footer>Copyright @2017 NCTUCS 交通大學資訊工程學系</footer>
+          </div>
+        );
+      }
 }
 
 export default Login;
