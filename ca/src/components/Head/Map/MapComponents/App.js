717fee2ebffd49dc4acccf9f2817fe28e4e0de02
danny <dammy021406@gmail.com[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Dnn>
2017-08-24 14:29:04 +0800

    test

--- ca/src/components/Head/Map/MapComponents/App.js
+++ ca/src/components/Head/Map/MapComponents/App.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import VisibleTodoList from '../containers/VisibleTodoList'
 import Footer from './Footer'
-
-const App = () => {
-        return(
-            <div>
-                <VisibleTodoList />
-                <Footer/>
-            </div>
-        )
+class App extends React.Component{
+    render(){return(
+        <div>
+            <VisibleTodoList />
+            <Footer/>
+        </div>
+    )
+    }
 }
 
 export default App
