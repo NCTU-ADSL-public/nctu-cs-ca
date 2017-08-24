1ec6d89b1b73e9cfea06340229fef577da06bea0
danny <dammy021406@gmail.com[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Dnn>
2017-08-24 14:51:04 +0800

    test

--- ca/src/components/Head/Map/MapComponents/App.js
+++ ca/src/components/Head/Map/MapComponents/App.js
@@ -9,12 +9,66 @@ class App extends React.Component{
     }
     render(){
             return(
-            <div>
-                <VisibleTodoList grad={this.props.grad} sem={this.props.sem}/>
-                <Footer/>
+                <div>
+                <div className="grades">
+                    <div className="grade" id="grade-1">
+                        <div className="grade-num"><h3>大一(104)</h3></div>
+                        <div className="session">
+                            <div  className="up-session">
+                                <VisibleTodoList grad="1" sem="1"/>
+                            </div>
+                            <div className="down-session">
+                                <VisibleTodoList grad="1" sem="2"/>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className="grade" id="grade-2">
+                        <div className="grade-num"><h3>大二(105)</h3></div>
+                        <div className="session">
+                            <div className="up-session">
+                                <VisibleTodoList grad="2" sem="1"/>
+
+                            </div>
+                            <div className="down-session">
+                                <VisibleTodoList grad="2" sem="2"/>
+
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className="grade grade-cur" id="grade-3">
+                        <div className="grade-num"><h3>大三(106)</h3></div>
+                        <div className="session">
+                            <div className="up-session session-cur">
+                                <VisibleTodoList grad="3" sem="1"/>
+                            </div>
+                            <div className="down-session">
+                                <VisibleTodoList grad="3" sem="2"/>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className="grade" id="grade-4">
+                        <div className="grade-num"><h3>大四(107)</h3></div>
+                        <div className="session">
+                            <div className="up-session">
+                                <VisibleTodoList grad="4" sem="1"/>
+                            </div>
+                            <div className="down-session">
+                                <VisibleTodoList grad="4" sem="2"/>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="mods"><Footer/></div>
             </div>
         )
     }
 }
-
 export default App
--- ca/src/components/Head/Map/MapComponents/Map.js
+++ ca/src/components/Head/Map/MapComponents/Map.js
@@ -62,64 +62,7 @@ class Map extends React.Component{
 
                 <div className="Map-Row">
                     <Provider store={store}>
-                    <div className="grades">
-                        <div className="grade" id="grade-1">
-                            <div className="grade-num"><h3>大一(104)</h3></div>
-                            <div className="session">
-                                <div  className="up-session">
-                                        <App grad="1" sem="1"/>
-                                </div>
-                                <div className="down-session">
-                                        <App grad="1" sem="2"/>
-                                </div>
-                            </div>
-                        </div>
-
-
-                        <div className="grade" id="grade-2">
-                            <div className="grade-num"><h3>大二(105)</h3></div>
-                            <div className="session">
-                                <div className="up-session">
-                                        <App grad="2" sem="1"/>
-
-                                </div>
-                                <div className="down-session">
-                                        <App grad="2" sem="2"/>
-
-                                </div>
-                            </div>
-                        </div>
-
-
-                        <div className="grade grade-cur" id="grade-3">
-                            <div className="grade-num"><h3>大三(106)</h3></div>
-                            <div className="session">
-                                <div className="up-session session-cur">
-                                        <App grad="3" sem="1"/>
-                                </div>
-                                <div className="down-session">
-                                        <App grad="3" sem="2"/>
-                                </div>
-                            </div>
-                        </div>
-
-
-                        <div className="grade" id="grade-4">
-                            <div className="grade-num"><h3>大四(107)</h3></div>
-                            <div className="session">
-                                <div className="up-session">
-                                        <App grad="4" sem="1"/>
-                                </div>
-                                <div className="down-session">
-                                        <App grad="4" sem="2"/>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-
-                    <div className="mods">
-                    </div>
+                        <App/>
                     </Provider>
                 </div>
             </div>
