f637edb72690416374844744b28cb0eeb5be5d76
danny <dammy021406@gmail.com[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Dnn>
2017-08-30 22:09:49 +0800

    test

--- ca/src/components/Head/Map/MapComponents/Todo.js
+++ ca/src/components/Head/Map/MapComponents/Todo.js
@@ -7,33 +7,41 @@ const optionsCursorTrueWithMargin = {
     followCursor: true,
     shiftX: 20,
     shiftY: 0
-}
-
+};
+{/*<div className="course">*/}
+    {/*<ReactHover*/}
+        {/*options={optionsCursorTrueWithMargin}>*/}
+        {/*<ReactHover.Trigger>*/}
+            {/*<div className="course-btn"*/}
+                 {/*style={{*/}
+                     {/*background: pre_flag ? '#61070d' : '#616161'*/}
+                 {/*}}*/}
+                 {/*onClick={onClick}>*/}
+                {/*{cosCame}*/}
+            {/*</div>*/}
+        {/*</ReactHover.Trigger>*/}
+        {/*<ReactHover.Hover>*/}
+            {/*<h1> I am hover HTML </h1>*/}
+        {/*</ReactHover.Hover>*/}
+    {/*</ReactHover>*/}
+{/*</div>*/}
 
 const Todo = ({ onClick, pre_flag, cosCame }) => (
-    <div className="course">
-        <ReactHover
-            options={optionsCursorTrueWithMargin}>
-            <ReactHover.Trigger>
-                    <div className="course-btn"
-                         style={{
-                             background: pre_flag ? '#61070d' : '#616161'
-                         }}
-                         onClick={onClick}>
-                        {cosCame}
-                    </div>
-            </ReactHover.Trigger>
-            <ReactHover.Hover>
-                <h1> I am hover HTML </h1>
-            </ReactHover.Hover>
-        </ReactHover>
-    </div>
-)
+    <ReactHover
+        options={optionsCursorTrueWithMargin}>
+        <ReactHover.Trigger>
+            <h1 style={{background: '#abbcf1', width: '200px'}}> Hover on me </h1>
+        </ReactHover.Trigger>
+        <ReactHover.Hover>
+            <h1> I am hover HTML </h1>
+        </ReactHover.Hover>
+    </ReactHover>
+);
 
 Todo.propTypes = {
     onClick: PropTypes.func.isRequired,
     completed: PropTypes.bool.isRequired,
     cos_cname: PropTypes.string.isRequired
-}
+};
 
 export default Todo
