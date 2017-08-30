38fcf42fd0e70c1d8b2e7b5908c6b9cc7eed05a0
danny <dammy021406@gmail.com[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Dnn>
2017-08-30 21:58:44 +0800

    test

--- ca/src/components/Head/Map/MapComponents/Todo.js
+++ ca/src/components/Head/Map/MapComponents/Todo.js
@@ -11,23 +11,25 @@ const optionsCursorTrueWithMargin = {
 
 
 const Todo = ({ onClick, pre_flag, cosCame }) => (
-    <ReactHover
-        options={optionsCursorTrueWithMargin}>
-        <ReactHover.Trigger>
-            <div className="course">
-                <div className="course-btn"
-                     style={{
-                         background: pre_flag ? '#61070d' : '#616161'
-                     }}
-                     onClick={onClick}>
-                    {cosCame}
+    <div>
+        <ReactHover
+            options={optionsCursorTrueWithMargin}>
+            <ReactHover.Trigger>
+                <div className="course">
+                    <div className="course-btn"
+                         style={{
+                             background: pre_flag ? '#61070d' : '#616161'
+                         }}
+                         onClick={onClick}>
+                        {cosCame}
+                    </div>
                 </div>
-            </div>
-        </ReactHover.Trigger>
-        <ReactHover.Hover>
-            <h1> I am hover HTML </h1>
-        </ReactHover.Hover>
-    </ReactHover>
+            </ReactHover.Trigger>
+            <ReactHover.Hover>
+                <h1> I am hover HTML </h1>
+            </ReactHover.Hover>
+        </ReactHover>
+    </div>
 )
 
 Todo.propTypes = {
