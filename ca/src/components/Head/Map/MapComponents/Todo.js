92936f44dd67670d078ccb94b664966181fc64b0
danny <dammy021406@gmail.com[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Dnn>
2017-08-30 23:34:21 +0800

    test

--- ca/src/components/Head/Map/MapComponents/Todo.js
+++ ca/src/components/Head/Map/MapComponents/Todo.js
@@ -10,7 +10,7 @@ const Todo = ({ onClick, pre_flag, cosCame }) => (
                              style={{
                                  background: pre_flag ? '#61070d' : '#616161'
                              }}
-                             onHoverChanged={onClick}>
+                             onClick={onClick}>
                             {cosCame}
                         </div>
                     </ReactHoverObserver>
