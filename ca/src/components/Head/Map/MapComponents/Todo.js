d38919b08ec9dad4e0f689efb7267a5168bd9cd5
danny <dammy021406@gmail.com[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Dnn>
2017-09-10 18:38:22 +0800

    button hover

--- ca/src/components/Head/Map/MapComponents/Todo.js
+++ ca/src/components/Head/Map/MapComponents/Todo.js
@@ -9,29 +9,12 @@ const optionsCursorTrueWithMargin = {
     shiftX: 20,
     shiftY: 0
 }
-class Todo extends React.Component{
+const Todo = ({ onClick, completed , pre_flag, cosCame }) =>(
 
-
-    constructor({ onClick, completed , pre_flag, cosCame }){
-        super({ onClick, completed , pre_flag, cosCame});
-    }
-
-    state = {
-        isMouseover:false,
-    }
-
-    handleHover(){
-        this.setState({
-            isMouseover:!this.state.isMouseover,
-        })
-    }
-
-    render(){
-        return(
             <ReactHover
                 options={optionsCursorTrueWithMargin}>
                 <ReactHover.Trigger>
-                    <div className="course" onMouseOver={() =>this.handleHover()} onMouseOut={() =>this.handleHover()}>
+                    <div className="course" >
                         <MuiThemeProvider>
                             <FlatButton className="course-btn"
                                         backgroundColor="#616161"
@@ -43,13 +26,12 @@ class Todo extends React.Component{
                                             letterSpacing: "1px"
                                         }}
                                         style={{
-                                            overflow:this.state.isMouseover?"visible":"hidden",
-                                            visibility:this.props.completed?"hidden":"visible" ,
-                                            border: this.props.pre_flag ? "solid 2px #611505":"#616161",
+                                            visibility:completed?"hidden":"visible" ,
+                                            border: pre_flag ? "solid 2px #611505":"#616161",
                                             padding: 0,
                                         }}
-                                        label={this.props.cosCame}
-                                        onClick={this.props.onClick}>
+                                        label={cosCame}
+                                        onClick={onClick}>
 
                             </FlatButton>
                         </MuiThemeProvider>
@@ -59,9 +41,7 @@ class Todo extends React.Component{
                     <h1>{this.props.cosCame}</h1>
                 </ReactHover.Hover>
             </ReactHover>
-        );
-    }
-}
+)
 
 
 Todo.PropTypes = {
