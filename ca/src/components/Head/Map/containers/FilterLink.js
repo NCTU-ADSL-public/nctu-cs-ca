b4048cc4e023bb44981bc1390b3f7038b067076c
danny <dammy021406@gmail.com[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Dnn>
2017-09-12 00:19:50 +0800

    Revert "test"
    This reverts commit cd38ce198f19d365c127ee7c6d6bad7f1a63feeb.

--- ca/src/components/Head/Map/containers/FilterLink.js
+++ ca/src/components/Head/Map/containers/FilterLink.js
@@ -13,16 +13,19 @@ const mapDispatchToProps = (dispatch, ownProps) => {
         onClick: () => {
             dispatch(setVisibilityFilter(ownProps.filter))
             if(ownProps.filter === "SHOW_ALL"){
+                console.log(ownProps.filter);
                 for(let j=0;j<ownProps.studentPasdata.length;j++){
                     dispatch(setPascos(ownProps.studentPasdata[j].cos_cname,0));
                 }
             }
             else if(ownProps.filter === "SHOW_COMPLETED"){
+                console.log(ownProps.filter);
                 for(let j=0;j<ownProps.studentPasdata.length;j++){
                     dispatch(setPascos(ownProps.studentPasdata[j].cos_cname,1));
                 }
             }
             else if(ownProps.filter === "SHOW_ACTIVE"){
+                console.log(ownProps.filter);
                 for(let j=0;j<ownProps.studentPasdata.length;j++){
                     dispatch(setPascos(ownProps.studentPasdata[j].cos_cname,2));
                 }
