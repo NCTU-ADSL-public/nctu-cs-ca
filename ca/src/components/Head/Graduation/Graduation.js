07e22f8356a0fc9f95b79272092687c540f91b29
danny <dammy021406@gmail.com[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Dnn>
2017-09-13 22:39:43 +0800

    test

--- ca/src/components/Head/Graduation/Graduation.js
+++ ca/src/components/Head/Graduation/Graduation.js
@@ -48,7 +48,7 @@ class UP extends React.Component {
     render(){
         /*set properties*/
         const options={
-            baseUrl:'http://localhost/students/score',
+            baseUrl:'http://csca.nctu.edu.tw/students/score',
             param:{
                 fid:0
             }
