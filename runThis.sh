88550bffea3efd462561ce0969a48c3c4786ebec
root <friedcosey@gmail.com>
2017-09-24 17:36:34 +0800

    exclude some files

--- runThis.sh
+++
@@ -1,9 +0,0 @@
-export BABEL_DISABLE_CACHE=1
-sudo git clone -b danny https://github.com/NCTU-ADSL-public/nctu-cs-ca.git
-sudo rm -rf src
-sudo rm -rf public
-sudo mv ./nctu-cs-ca/ca/src ./src
-sudo mv ./nctu-cs-ca/ca/public ./public
-sudo rm -rf nctu-cs-ca
-sudo npm run dev
-
