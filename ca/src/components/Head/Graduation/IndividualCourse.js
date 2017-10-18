030cd43ac359b4055d9dd3db7e64e0a0b74b7d6e
danny <dammy021406@gmail.com[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Dnn>
2017-10-18 23:27:20 +0800

    wrting graduationform components done

--- ca/src/components/Head/Graduation/IndividualCourse.js
+++ ca/src/components/Head/Graduation/IndividualCourse.js
@@ -11,7 +11,7 @@ export default class IndividualCourse extends React.Component {
                 <IndividualProgress grad={this.props.credit/this.props.total * 100}/>
                 <div id="little-title-title">
                     <div id="little-title-number"><font size={6} color='#338d68'>{this.props.credit}</font>/{this.props.total}</div>
-                    <div id="little-title-text" ><font size={this.props.fontflag?5:6}>{this.props.name}</font></div>
+                    <div id="little-title-text" ><font size={this.props.fontflag?5:6}>{this.props.title}</font></div>
                 </div>
 
             </div>
