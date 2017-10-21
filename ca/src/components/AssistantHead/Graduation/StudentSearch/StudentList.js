4c47bd4a14986fa851b86723b6dcda9c5d6af5ef
osinoyan <chopsticks1515@gmail.com>
2017-10-22 02:54:39 +0800

    Let Student searching works by passing props

--- ca/src/components/AssistantHead/Graduation/StudentSearch/StudentList.js
+++ ca/src/components/AssistantHead/Graduation/StudentSearch/StudentList.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React from 'react';
 import './StudentList.css';
 
+//for table
+import StudentTable from './StudentTable';
+
+//for multiTheme
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+const muiTheme = getMuiTheme({
+    palette: {
+        primary1Color: 'Transparent',
+        accent1Color: '#00AEAE',
+    },
+});
+
+//main
 export default class StudentList extends React.Component {
     render() {
         return (
@@ -16,60 +30,224 @@ class FilteredList extends React.Component {
         super(props, context);
 
         this.state = {
-            initialItems: [
-                "Apples",
-                "Broccoli",
-                "Chicken",
-                "Bacon",
-                "Eggs",
-                "Salmon",
-                "Granola",
-                "Bananas",
-                "Beer",
-                "Wine",
-                "Yogurt"
+            initStudents: [
+                {
+                    name: '流川楓',
+                    group: '網多',
+                    graduated: '已畢業',
+                },
+                {
+                    name: '余治杰',
+                    group: '資工B',
+                    graduated: '未畢業',
+                },
+                {
+                    name: '王冠升',
+                    group: '資工B',
+                    graduated: '未畢業',
+                },
+                {
+                    name: '陳冠廷',
+                    group: '資工A',
+                    graduated: '已畢業',
+                },
+                {
+                    name: '郭蕎',
+                    group: '資電',
+                    graduated: '已畢業',
+                },
+
+
             ],
-            items: []
+            students: [],
         };
 
         this.filterList = this.filterList.bind(this);
     }
 
     componentWillMount(){
-        this.setState({items: this.state.initialItems});
+        this.setState({students: this.state.initStudents});
     }
 
     filterList(event){
-        let updatedList = this.state.initialItems;
-        updatedList = updatedList.filter(function(item){
-            return item.toLowerCase().search(
+        let updatedList = this.state.initStudents;
+        updatedList = updatedList.filter(function(student){
+            return student.name.toLowerCase().search(
                 event.target.value.toLowerCase()) !== -1;
         });
-        this.setState({items: updatedList});
+        this.setState({students: updatedList});
     }
     render(){
         return (
-            <div className="filter-list">
-                <input type="text" placeholder="Search" onChange={this.filterList}/>
-                <List items={this.state.items}/>
+            <div>
+                <div className="filter-list">
+                    <input type="text" placeholder="Search" onChange={this.filterList}/>
+                </div>
+                <MuiThemeProvider muiTheme={muiTheme}>
+                    <StudentTable students={this.state.students}/>
+                </MuiThemeProvider>
+            </div>
+        );
+    }
+
+
+
+
+
+}
+
+/*
+class List extends React.Component {
+
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+            fixedHeader: true,
+            fixedFooter: true,
+            stripedRows: false,
+            showRowHover: false,
+            selectable: true,
+            multiSelectable: false,
+            enableSelectAll: false,
+            deselectOnClickaway: true,
+            showCheckboxes: false,
+            height: '500px',
+        };
+    }
+
+    render() {
+        return (
+
+            <div>
+
+                <Table
+                    height={this.state.height}
+                    fixedHeader={this.state.fixedHeader}
+                    fixedFooter={this.state.fixedFooter}
+                    selectable={this.state.selectable}
+                    multiSelectable={this.state.multiSelectable}
+                >
+                    <TableHeader
+                        displaySelectAll={this.state.showCheckboxes}
+                        adjustForCheckbox={this.state.showCheckboxes}
+                        enableSelectAll={this.state.enableSelectAll}
+                    >
+                        <TableRow>
+                            <TableHeaderColumn tooltip="開課學期">學期</TableHeaderColumn>
+                            <TableHeaderColumn tooltip="課程名稱">課程名稱</TableHeaderColumn>
+                            <TableHeaderColumn tooltip="開課系所">系所</TableHeaderColumn>
+                            <TableHeaderColumn tooltip="學生平均成績">平均成績</TableHeaderColumn>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody
+                        displayRowCheckbox={this.state.showCheckboxes}
+                        deselectOnClickaway={this.state.deselectOnClickaway}
+                        showRowHover={this.state.showRowHover}
+                        stripedRows={this.state.stripedRows}
+                    >
+                        {this.props.students.map((row, index) => (
+                            <TableRow key={index}>
+                                <TableRowColumn style={styles.tabColumn0}>{row.name}</TableRowColumn>
+                                <TableRowColumn style={styles.tabColumn1}>{row.group}</TableRowColumn>
+                                <TableRowColumn style={styles.tabColumn0}>{row.graduated}</TableRowColumn>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+
+
             </div>
         );
     }
 }
+*/
 
 
-class List extends React.Component {
-    render(){
-        return (
-            <ul>
-                {
-                    this.props.items.map(function(item) {
-                        return <li key={item}>{item}</li>
-                    })
-                }
-            </ul>
-        )
+/*
+const styles = {
+    propContainer: {
+        width: 200,
+        overflow: 'hidden',
+        margin: '20px auto 0',
+    },
+    propToggleHeader: {
+        margin: '20px auto 10px',
+    },
+    tabColumn0: {
+        background: '#ecfcf9',
+        border: '3px solid white',
+    },
+    tabColumn1: {
+        background: '#f9f9f9',
+        border: '3px solid white',
+    },
+};
+*/
+/*
+class StudentTable extends React.Component {
+
+
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+            fixedHeader: true,
+            fixedFooter: true,
+            stripedRows: false,
+            showRowHover: false,
+            selectable: true,
+            multiSelectable: false,
+            enableSelectAll: false,
+            deselectOnClickaway: true,
+            showCheckboxes: false,
+            height: '500px',
+        };
     }
-}
+
+    render() {
+        return (
+
+            <div>
+
+                <Table
+                    height={this.state.height}
+                    fixedHeader={this.state.fixedHeader}
+                    fixedFooter={this.state.fixedFooter}
+                    selectable={this.state.selectable}
+                    multiSelectable={this.state.multiSelectable}
+                >
+                    <TableHeader
+                        displaySelectAll={this.state.showCheckboxes}
+                        adjustForCheckbox={this.state.showCheckboxes}
+                        enableSelectAll={this.state.enableSelectAll}
+                    >
+                        <TableRow>
+                            <TableHeaderColumn tooltip="開課學期">學期</TableHeaderColumn>
+                            <TableHeaderColumn tooltip="課程名稱">課程名稱</TableHeaderColumn>
+                            <TableHeaderColumn tooltip="開課系所">系所</TableHeaderColumn>
+                            <TableHeaderColumn tooltip="學生平均成績">平均成績</TableHeaderColumn>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody
+                        displayRowCheckbox={this.state.showCheckboxes}
+                        deselectOnClickaway={this.state.deselectOnClickaway}
+                        showRowHover={this.state.showRowHover}
+                        stripedRows={this.state.stripedRows}
+                    >
+                        {this.props.students.map((row, index) => (
+                            <TableRow key={index}>
+                                <TableRowColumn style={styles.tabColumn0}>{row.name}</TableRowColumn>
+                                <TableRowColumn style={styles.tabColumn1}>{row.group}</TableRowColumn>
+                                <TableRowColumn style={styles.tabColumn0}>{row.graduated}</TableRowColumn>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
 
 
+            </div>
+        );
+    }
+
+}*/
---
+++ ca/src/components/AssistantHead/Graduation/StudentSearch/StudentTable.js
@@ -0,0 +1,107 @@
+import React, {Component} from 'react';
+import {
+    Table,
+    TableBody,
+    TableHeader,
+    TableHeaderColumn,
+    TableRow,
+    TableRowColumn,
+} from 'material-ui/Table';
+
+const styles = {
+    propContainer: {
+        width: 200,
+        overflow: 'hidden',
+        margin: '20px auto 0',
+    },
+    propToggleHeader: {
+        margin: '20px auto 10px',
+    },
+    tabColumn0: {
+        background: '#ecfcf9',
+        border: '3px solid white',
+    },
+    tabColumn1: {
+        background: '#f9f9f9',
+        border: '3px solid white',
+    },
+};
+
+/**
+ * A more complex example, allowing the table height to be set, and key boolean properties to be toggled.
+ */
+export default class StudentTable extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            fixedHeader: true,
+            fixedFooter: true,
+            stripedRows: false,
+            showRowHover: false,
+            selectable: true,
+            multiSelectable: false,
+            enableSelectAll: false,
+            deselectOnClickaway: true,
+            showCheckboxes: false,
+            height: '500px',
+        };
+    }
+
+    static defaultProps = {
+        students: [
+            {
+                name: '流川楓',
+                group: '網多',
+                graduated: '已畢業',
+            }]
+    };
+
+
+
+
+    render() {
+        return (
+
+            <div>
+
+                <Table
+                    height={this.state.height}
+                    fixedHeader={this.state.fixedHeader}
+                    fixedFooter={this.state.fixedFooter}
+                    selectable={this.state.selectable}
+                    multiSelectable={this.state.multiSelectable}
+                >
+                    <TableHeader
+                        displaySelectAll={this.state.showCheckboxes}
+                        adjustForCheckbox={this.state.showCheckboxes}
+                        enableSelectAll={this.state.enableSelectAll}
+                    >
+                        <TableRow>
+                            <TableHeaderColumn tooltip="學生姓名">學生姓名</TableHeaderColumn>
+                            <TableHeaderColumn tooltip="組別">組別</TableHeaderColumn>
+                            <TableHeaderColumn tooltip="畢業狀況">畢業狀況</TableHeaderColumn>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody
+                        displayRowCheckbox={this.state.showCheckboxes}
+                        deselectOnClickaway={this.state.deselectOnClickaway}
+                        showRowHover={this.state.showRowHover}
+                        stripedRows={this.state.stripedRows}
+                    >
+                        {this.props.students.map((row, index) => (
+                            <TableRow key={index}>
+                                <TableRowColumn style={styles.tabColumn0}>{row.name}</TableRowColumn>
+                                <TableRowColumn style={styles.tabColumn1}>{row.group}</TableRowColumn>
+                                <TableRowColumn style={styles.tabColumn0}>{row.graduated}</TableRowColumn>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+
+
+            </div>
+        );
+    }
+
+}
