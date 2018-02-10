ba420a74dc1e6fd721620cd67acca274a0495884
danny <dammy021406@gmail.com[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Dnn>
2018-02-10 20:09:37 +0800

    map modify

--- ca/src/components/Head/Map/MapComponents/Map.js
+++ ca/src/components/Head/Map/MapComponents/Map.js
@@ -177,18 +177,53 @@ class Map extends React.Component {
                               basic = 50
                               ny = basic
                           }
-                          else if(this.props.data[i].cos_cname.match('微積分')!==null || this.props.data[i].cos_cname.match('機率')!==null || this.props.data[i].cos_cname.match('線性代數')!==null|| this.props.data[i].cos_cname.match('離散數學')!==null){
-                              math = 200
-                              ny = math
-                          }
                           else if(this.props.data[i].cos_cname.match('作業系統概論')!==null ) {
                               ny = 600
                           }
                           else if(this.props.data[i].cos_cname.match('基礎程式設計')!==null ) {
                               ny = 280
                           }
+                          else if(this.props.data[i].cos_cname.match('訊號與系統')!==null ) {
+                              ny = 150
+                          }
+                          else if(this.props.data[i].cos_cname.match('數值方法')!==null ) {
+                              ny = 150
+                          }
+                          else if(this.props.data[i].cos_cname.match('影像處理概論')!==null ) {
+                              ny = 200
+                          }
+                          else if(this.props.data[i].cos_cname.match('計算機圖學概論')!==null ) {
+                              ny = 240
+                          }
+                          else if(this.props.data[i].cos_cname.match('軟硬體協同設計概論與實作')!==null ) {
+                              ny = 320
+                          }
+                          else if(this.props.data[i].cos_cname.match('計算機概論')!==null ) {
+                              ny = 350
+                          }
+                          else if(this.props.data[i].cos_cname.match('正規語言')!==null ) {
+                              ny = 200
+                          }
+                          else if(this.props.data[i].cos_cname.match('編譯器設計概論')!==null ) {
+                              ny = 200
+                          }
+                          else if(this.props.data[i].cos_cname.match('資料結構')!==null ) {
+                              ny = 280
+                          }
+                          else if(this.props.data[i].cos_cname.match('資訊工程研討')!==null ) {
+                              computer += 200
+                              ny = computer
+                          }
                           else if(this.props.data[i].cos_cname.match('資訊工程專題')!==null ) {
                               ny = 280
+                          }
+
+                          else if(this.props.data[i].cos_cname.match('計算機網路概論')!==null ) {
+                              ny = 700
+                          }
+                          else if(this.props.data[i].cos_cname.match('微積分')!==null || this.props.data[i].cos_cname.match('機率')!==null || this.props.data[i].cos_cname.match('線性代數')!==null|| this.props.data[i].cos_cname.match('離散數學')!==null){
+                              math = 200
+                              ny = math
                           }
                           else {
                               computer = 350
