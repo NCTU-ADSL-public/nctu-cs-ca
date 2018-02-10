8af602275ac26899b8ab9a97b3f4a065f5f2a6ac
Johnson Chao <chao.hs1031@gmail.com>
2018-02-10 23:40:29 +0800

    Fix indent

--- ca/src/components/Login/Login.js
+++ ca/src/components/Login/Login.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import LoginButtom from './LoginButtom.js';
-import './Login.css';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import FadeIn from 'react-fade-in';
 import axios from 'axios';
 import {ToastContainer, ToastStore} from 'react-toasts';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import './Login.css';
+import LoginButtom from './LoginButtom.js';
 import PopoverExampleAnimation from './popover';
 
-
 class Login extends React.Component {
-
 
     getLoginstatus(){
         let _this = this;
@@ -37,36 +36,36 @@ class Login extends React.Component {
         this.getLoginstatus();
     }
     componentDidMount() {
-             if (this.state.LoginMessage_state === "1")
-                ToastStore.error("Oops you are not cs student !");
-             else if(this.state.LoginMessage_state === "2")
-                 ToastStore.error("Please login first")
+        if (this.state.LoginMessage_state === "1")
+            ToastStore.error("Oops you are not cs student !");
+        else if(this.state.LoginMessage_state === "2")
+            ToastStore.error("Please login first")
     }
 
     render() {
         return (
-          <div className="Login"  >
-              <ToastContainer store={ToastStore}/>
+            <div className="Login"  >
+                <ToastContainer store={ToastStore}/>
                 <div id="AjustToggleButtom"> </div>
                 <div className="Login-header"   ref="tip">
 
-                <FadeIn>
-                    <div id="rectangle"> </div>
-                    <div id="eng-title"><div id="h11">NCTU Curriculum Assistant</div></div>
-                    <div id="ch-title"><div id="h22">交大資工線上助理</div></div>
+                    <FadeIn>
+                        <div id="rectangle"> </div>
+                        <div id="eng-title"><div id="h11">NCTU Curriculum Assistant</div></div>
+                        <div id="ch-title"><div id="h22">交大資工線上助理</div></div>
 
-                    <div className = "Login-login">
-                        <MuiThemeProvider>
-                    <PopoverExampleAnimation />
-                        </MuiThemeProvider>
-                      <MuiThemeProvider>
-                        <LoginButtom />
-                      </MuiThemeProvider>
-                    </div>
+                        <div className = "Login-login">
+                            <MuiThemeProvider>
+                                <PopoverExampleAnimation />
+                            </MuiThemeProvider>
+                            <MuiThemeProvider>
+                                <LoginButtom />
+                            </MuiThemeProvider>
+                        </div>
 
-                </FadeIn>
+                    </FadeIn>
                 </div>
-          </div>
+            </div>
         );
       }
 }
