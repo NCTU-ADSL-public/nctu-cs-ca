8691b0fb1bed1a3e10bc7cb53bb432c1f9985a94
Johnson Chao <chao.hs1031@gmail.com>
2018-02-10 22:56:52 +0800

    Move test data to json files.

---
+++ ca/src/components/Head/FakeData/index.js
@@ -0,0 +1,13 @@
+import GraduationItems from './GraduationItems.json'
+import GraduationItems_Revised from './GraduationItems_Revised.json'
+import Course from './Course.json'
+import CoursePass from './CoursePass.json'
+import PrintData from './PrintData.json'
+
+export default {
+    GraduationItems,
+    GraduationItems_Revised,
+    Course,
+    CoursePass,
+    PrintData
+}
