739a8fb8de99d7f7e30042cc01a7fa6ca8e2550e
Johnson Chao <chao.hs1031@gmail.com>
2018-02-11 00:25:25 +0800

    Replace test data in assistants with json file

--- ca/src/components/AssistantHead/Graduation/index.js
+++ ca/src/components/AssistantHead/Graduation/index.js
@@ -10,6 +10,8 @@ import ScrollableInkTabBar from 'rc-tabs/lib/ScrollableInkTabBar';
 import StudentList from './StudentSearch/StudentList';
 import StudentInform from './StudentInform/StudentSelList';
 
+import FakeData from '../../FakeData'
+
 export default class index extends React.Component {
 
     constructor(props) {
@@ -18,72 +20,7 @@ export default class index extends React.Component {
         this.state = {
             activeKey: '1',
             start: 0,
-            initStudents: [
-                {
-                    student_id: '0316099',
-                    sname: '資料錯誤',
-                    program: '網多',
-                    graduate: "0",
-                    graduate_submit: "0",
-                },
-                {
-                    student_id: '0316030',
-                    sname: '余治杰',
-                    program: '資工B',
-                    graduate: "0",
-                    graduate_submit: "1",
-                },
-                {
-                    student_id: '0316031',
-                    sname: '王冠升',
-                    program: '資工B',
-                    graduate: "1",
-                    graduate_submit: "1",
-                },
-                {
-                    student_id: '0316132',
-                    sname: '郭毓梁',
-                    program: '資工A',
-                    graduate: "0",
-                    graduate_submit: "1",
-                },
-                {
-                    student_id: '0316033',
-                    sname: '趙賀笙',
-                    program: '資工A',
-                    graduate: "1",
-                    graduate_submit: "2",
-                },
-                {
-                    student_id: '0316034',
-                    sname: '王于哲',
-                    program: '資工B',
-                    graduate: "0",
-                    graduate_submit: "2",
-                },
-                {
-                    student_id: '0316235',
-                    sname: '陳奕安',
-                    program: '資工B',
-                    graduate: "0",
-                    graduate_submit: "0",
-                },
-                {
-                    student_id: '0316036',
-                    sname: '陳冠廷',
-                    program: '網多',
-                    graduate: "1",
-                    graduate_submit: "0",
-                },
-                {
-                    student_id: '0316037',
-                    sname: '郭蕎',
-                    program: '資電',
-                    graduate: "0",
-                    graduate_submit: "0",
-                },
-
-            ],
+            initStudents: FakeData.StudentList,
             loading: true,
         };
     }
@@ -107,26 +44,26 @@ export default class index extends React.Component {
     };
 
     render() {
-            return (
-                <div>
-                    <Tabs
-                        renderTabBar={() => <ScrollableInkTabBar onTabClick={this.onTabClick}/>}
-                        renderTabContent={() => <TabContent/>}
-                        activeKey={this.state.activeKey}
-                        onChange={this.onChange}
-                    >
-                        <TabPane tab={`學生清單`} key="1">
+        return (
+            <div>
+                <Tabs
+                    renderTabBar={() => <ScrollableInkTabBar onTabClick={this.onTabClick}/>}
+                    renderTabContent={() => <TabContent/>}
+                    activeKey={this.state.activeKey}
+                    onChange={this.onChange}
+                >
+                    <TabPane tab={`學生清單`} key="1">
 
-                            <StudentList students={this.state.initStudents}/>
-                        </TabPane>
-                        <TabPane tab={`預審通知`} key="2">
-                            <StudentInform
-                                students={this.state.initStudents}
-                                idCard={this.props.idCard}
-                            />
-                        </TabPane>
-                    </Tabs>
-                </div>
-            );
+                        <StudentList students={this.state.initStudents}/>
+                    </TabPane>
+                    <TabPane tab={`預審通知`} key="2">
+                        <StudentInform
+                            students={this.state.initStudents}
+                            idCard={this.props.idCard}
+                        />
+                    </TabPane>
+                </Tabs>
+            </div>
+        );
     }
 }
