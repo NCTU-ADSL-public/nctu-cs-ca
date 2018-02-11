5c9248921bdec8b017d77213741aa23181ea5577
Johnson Chao <chao.hs1031@gmail.com>
2018-02-12 01:30:40 +0800

    Move files.

--- ca/src/index.js
+++ ca/src/index.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import Router from './components/Router'
-// import registerServiceWorker from './registerServiceWorker';
+import Router from './Router'
 
 ReactDOM.render(<Router />, document.getElementById('root'))
-
-// registerServiceWorker();
