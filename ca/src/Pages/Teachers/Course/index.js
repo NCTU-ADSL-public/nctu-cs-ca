94b0616bd16b2a6174925685d18419e5ec0243fd
osino <chopsticks1515@gmail.com>
2018-03-12 00:27:26 +0800

    Teachers/Family initial

--- ca/src/Pages/Teachers/Course/index.js
+++ ca/src/Pages/Teachers/Course/index.js
@@ -105,7 +105,6 @@ export default class index extends React.Component {
   componentWillReceiveProps(nextProps){
     if(this.props.tid !== nextProps.tid){
 
-      console.log("index receive props tid");
       axios.post('/professors/courseInfo/courses', {
         id: nextProps.tid,
       }).then(res => {
