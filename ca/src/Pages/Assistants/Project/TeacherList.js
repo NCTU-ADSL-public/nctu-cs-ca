de9f76c050a32764e75cfc116d7c3101296d4135
Johnson Chao <chao.hs1031@gmail.com>
2018-04-27 13:34:57 +0800

    Add send mail button

--- ca/src/Pages/Assistants/Project/TeacherList.js
+++ ca/src/Pages/Assistants/Project/TeacherList.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import {Grid, Row, Col} from 'react-bootstrap'
-import {Form, FormGroup, ControlLabel, FormControl} from 'react-bootstrap'
-import Table from '../../../Components/Table'
+import {Grid, Row, Col, Form, FormGroup, ControlLabel, FormControl} from 'react-bootstrap'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import RaisedButton from 'material-ui/RaisedButton'
+import Email from 'material-ui/svg-icons/communication/email'
 
+import Table from '../../../Components/Table'
 import FakeData from '../../../Resources/FakeData/index'
 
 class TeacherList extends React.Component {
@@ -18,6 +20,7 @@ class TeacherList extends React.Component {
                 <tr>
                   <th>教授</th>
                   <th>已收學生人數</th>
+                  <th></th>
                 </tr>
               </thead>
               <tbody>
@@ -26,6 +29,11 @@ class TeacherList extends React.Component {
                 <tr>
                   <td>{teacher.tname}</td>
                   <td>{teacher.scount}</td>
+                  <td>
+                    <MuiThemeProvider>
+                      <RaisedButton label='Send Mail!' icon={<Email />} />
+                    </MuiThemeProvider>
+                  </td>
                 </tr>
               ))}
               </tbody>
