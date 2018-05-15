4d3051ad1a9feb7e6f8a957f3f4854c5722b7ad6
danny <dammy021406@gmail.com[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Dnn>
2018-05-15 10:45:42 +0800

    connect to back end

--- ca/src/Pages/Students/ProjectHome/Edit/index.js
+++ ca/src/Pages/Students/ProjectHome/Edit/index.js
@@ -11,9 +11,9 @@ class Edit extends React.Component {
     this.onChange = this.onChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
     this.state = {
-      title:'NCTUCS BOT',
-      link:'/ttt',
-      introduce:'<h1>Hey</h1><br/><br/><br/><br/><br/>'
+      title: this.props.show.title,
+      link: this.props.show.url,
+      ckeditorContent: this.props.show.introduce
     }
   }
 
@@ -23,26 +23,23 @@ class Edit extends React.Component {
     })
   }
 
-  componentWillMount () {
-    this.setState({
-      ckeditorContent: 'this.props.event.content'
-    })
-  }
-
   handleSubmit (event) {
     let _this = this
-
-    if (_this.state.title !== '') {
+    event.preventDefault()
+    if (_this.refs.title.title !== '') {
       event.preventDefault()
-      axios.post('/students/ProjectEdit', {
-        title: _this.refs.title.value,
-        url: _this.refs.url.value,
-        content: _this.state.ckeditorContent
+      axios.post('/students/editProject', {
+        student_id: _this.props.studentProfile.student_id,
+        tname: _this.props.project.tname,
+        research_title: _this.props.show.title,
+        new_title: _this.refs.title.value,
+        new_link: _this.refs.url.value,
+        new_intro: _this.state.ckeditorContent
       }).then(res => {
+        this.props.onclick()
       }).catch(err => {
         console.log(err)
       })
-
     }
   }
 
@@ -50,15 +47,20 @@ class Edit extends React.Component {
     return (
       <div className='container bg-white'>
         <div className='row'>
-          <div className='col-md-9 offset-1'>
+          <div className='col-md-12 offset-1'>
             <h1 className='title offset-1'>
               編輯專題主頁
             </h1>
+            <br />
+            <br />
             <form>
               <LabeledInput label='圖片'>
-                <input accept='image/*' type='file' hidden />
-                <div className='text-center clickable upload-picture' >
-                  點選以上傳,建議800x400以達最佳效果(需小於2MB)
+                <div className='upload-btn-wrapper' style={{cursor: 'pointer'}}>
+                  <button className='btn'> 上傳圖片</button>
+                  <input accept='image/*' type='file' hidden />
+                  <div className='text-center clickable upload-picture' >
+                    點選以上傳,建議800x400以達最佳效果(需小於2MB)
+                  </div>
                 </div>
               </LabeledInput>
               <LabeledInput label='主題'>
@@ -70,7 +72,9 @@ class Edit extends React.Component {
                 <input ref='url' className='form-control' placeholder='選填' type='text' value={this.state.url} />
               </LabeledInput>
               <LabeledInput label='簡介'>
-                <CKEditor activeClass='p10' content={this.state.ckeditorContent} events={{'change': this.onChange}} />
+                <div >
+                  <CKEditor style={{height: '50vh'}} activeClass='p10' content={this.state.ckeditorContent} events={{'change': this.onChange}} />
+                </div>
               </LabeledInput>
               <div className='col-12 text-right'>
                 <button type='submit' className='btn btn-success btn-large' onClick={this.handleSubmit}>送出</button>
--- ca/src/Pages/Students/ProjectHome/index.js
+++ ca/src/Pages/Students/ProjectHome/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Show from './Show/index'
 import Edit from  './Edit'
-
+import axios from 'axios'
 
 import PageWrapper from '../../../Components/PageWrapper'
 
@@ -12,18 +12,94 @@ export default class index extends React.Component {
     super(props);
     this.changeState = this.changeState.bind(this)
     this.state = {
-      title:'NCTUCS BOT',
-      link:'/ttt',
-      introduce:'<h1>Hey</h1><br/><br/><br/><br/><br/>',
+      project: this.props.project,
+      Show: {
+        title:this.props.project.research_title,
+        url: '/ttt',
+        introduce: '<h1>Hey! 這是我的簡介標題嘿嘿</h1>\n' +
+        '\n' +
+        '<p><em>我還可以有鞋字體喔~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~&nbsp; &nbsp;&nbsp;</em></p>\n' +
+        '\n' +
+        '<table border="1" cellpadding="1" cellspacing="1" style="width:500px">\n' +
+        '\t<tbody>\n' +
+        '\t\t<tr>\n' +
+        '\t\t\t<td>還有表格</td>\n' +
+        '\t\t\t<td>ㄏ ㄏ</td>\n' +
+        '\t\t</tr>\n' +
+        '\t\t<tr>\n' +
+        '\t\t\t<td>6666</td>\n' +
+        '\t\t\t<td>666</td>\n' +
+        '\t\t</tr>\n' +
+        '\t\t<tr>\n' +
+        '\t\t\t<td>&nbsp;</td>\n' +
+        '\t\t\t<td>&nbsp;</td>\n' +
+        '\t\t</tr>\n' +
+        '\t</tbody>\n' +
+        '</table>\n' +
+        '\n' +
+        '<p>&nbsp;</p>\n' +
+        '\n' +
+        '<p><a id="你好" name="你好"></a></p>\n' +
+        '\n' +
+        '<h2><strong>&nbsp; 這樣介紹還OK嗎?</strong></h2>\n' +
+        '\n' +
+        '<p><img alt="" src="https://www.cs.nctu.edu.tw/cswebsite/assets/upload/people/_199.png" style="height:180px; width:140px" /></p>\n' +
+        '\n' +
+        '<p>&nbsp;</p>\n' +
+        '\n' +
+        '<p>發現我長得有點像豆豆先生</p>\n' +
+        '\n' +
+        '<p>&nbsp;</p>\n' +
+        '\n' +
+        '<p>&nbsp;</p>\n' +
+        '\n' +
+        '<p>&nbsp;</p>\n' +
+        '\n' +
+        '<p>&nbsp;</p>\n' +
+        '\n' +
+        '<p>&nbsp;</p>',
+      },
       state:'show'
     }
   }
 
+  componentWillMount () {
+    this.fetchData()
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.fetchData()
+  }
+
+  fetchData () {
+    let _this = this
+    axios.post('/students/projectPage', {
+      student_id: _this.props.studentProfile.student_id,
+    })
+      .then(res => {
+        _this.setState({
+          Show:{
+            url:res.data.filter(t=>t.research_title === _this.state.project.research_title).link,
+            title:res.data.filter(t=>t.research_title === _this.state.project.research_title).research_title,
+            introduce:res.data.filter(t=>t.research_title === _this.state.project.research_title).intro
+          }
+        })
+      })
+      .catch(err => {
+        console.log(err)
+      })
+
+  }
+
   changeState () {
     let state =  this.state.state === 'edit'?'show':'edit'
+    if(state === 'show'){
+      this.fetchData()
+    }
     this.setState({
       state:state
     })
+    let _this = this
   }
 
   searchCallback = (item) => {
@@ -33,7 +109,7 @@ export default class index extends React.Component {
 
     return (
       <PageWrapper>
-        {this.state.state === 'edit'?<Edit/>:<Show/>}
+        {this.state.state === 'edit'?<Edit project={this.state.project} studentProfile={this.props.studentProfile} onclick = {this.changeState} show={this.state.Show}/>:<Show show={this.state.Show}/>}
 
         <div className='fixed-menu fixed' style={{display:`${this.state.state === 'edit'?'none':''}`}}>
           <div className='container'>
@@ -42,7 +118,6 @@ export default class index extends React.Component {
                 {this.state.state === 'edit'?'送出':'編輯'}
               </button>
             </div>
-            <div> </div>
           </div>
         </div>
       </PageWrapper>
