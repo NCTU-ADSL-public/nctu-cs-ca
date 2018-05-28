dab5f8a36c9fb5ff4532ddc50186bef6aac56953
osino <chopsticks1515@gmail.com>
2018-05-29 04:20:42 +0800

    Separate GroupApply & GroupList into 2 pages

--- ca/src/Pages/Teachers/Group/Group.js
+++ ca/src/Pages/Teachers/Group/Group.js
@@ -3,16 +3,42 @@ import React from 'react'
 import GroupList from './GroupList'
 import GroupApply from './GroupApply'
 
+// for tabs
+import 'rc-tabs/assets/index.css'
+import '../../../Components/ca-rc-tabs.css'
+import Tabs, { TabPane } from 'rc-tabs'
+import TabContent from 'rc-tabs/lib/SwipeableTabContent'
+import ScrollableInkTabBar from 'rc-tabs/lib/ScrollableInkTabBar'
+
 class Group extends React.Component {
   constructor (props) {
     super(props)
-    this.state = {}
+    this.state = {
+      activeKey: '1',
+      start: 0
+    }
   }
+  onChange = (activeKey) => {
+    this.setState({
+      activeKey,
+    });
+  };
   render () {
     return (
       <div>
-        <GroupApply idCard={this.props.idCard} />
-        <GroupList idCard={this.props.idCard} />
+        <Tabs
+          renderTabBar={() => <ScrollableInkTabBar onTabClick={this.onTabClick} />}
+          renderTabContent={() => <TabContent />}
+          activeKey={this.state.activeKey}
+          onChange={this.onChange}
+        >
+          <TabPane tab={`專題申請`} key='1'>
+            <GroupApply idCard={this.props.idCard} />
+          </TabPane>
+          <TabPane tab={`專題列表`} key='2'>
+            <GroupList idCard={this.props.idCard} />
+          </TabPane>
+        </Tabs>
       </div>
     )
   }
