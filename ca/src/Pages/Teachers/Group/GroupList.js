398a4464c954e794572ad702cbcb783343d68ebc
danny <dammy021406@gmail.com[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Dnn>
2018-06-05 15:32:00 +0800

    modify heyeh

--- ca/src/Pages/Teachers/Group/GroupList.js
+++ ca/src/Pages/Teachers/Group/GroupList.js
@@ -238,7 +238,8 @@ class GroupList extends React.Component {
   render () {
     const tn = this.state.total_number
     return (
-      <Grid>
+      this.state.groupListlength !== this.state.groupList.length ? ''
+      : <Grid>
         <SwipeableViews index={this.state.index} onChangeIndex={this.handleChangeIndex} >
           <div>
             <Row>
