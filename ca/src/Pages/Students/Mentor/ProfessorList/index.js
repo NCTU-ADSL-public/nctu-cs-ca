421b88b0ddc062e55ba17f1949e05d57d90380a0
王冠升 <wangguansheng@wrl89-210.nchc.org.tw>
2018-07-17 16:33:09 +0800

    modify professors page

--- ca/src/Pages/Students/Mentor/ProfessorList/index.js
+++ ca/src/Pages/Students/Mentor/ProfessorList/index.js
@@ -20,6 +20,7 @@ class ProfessorsList extends React.Component {
 
 const getVisibleProfessors = (data, filterInput) => {
   if (data.length === 0) return []
+  console.log(data)
   let updatedList = data.filter((item) => {
     return (
       (item.tname.toLowerCase().search(
