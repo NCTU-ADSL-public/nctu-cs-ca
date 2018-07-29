90e4c31e9d23772d001d508e7385b6672d324d94
marmot0814 <s1357andy871207@gmail.com>
2018-07-29 14:23:37 +0800

    merge, program filter done

--- ca/src/Pages/Assistants/Project/StudentsItem.js
+++ ca/src/Pages/Assistants/Project/StudentsItem.js
@@ -4,10 +4,9 @@ import StudentsTable from './StudentsTable'
 import TextField from 'material-ui/TextField';
 import Chip from 'material-ui/Chip';
 
-import { blue400, yellow400, red400, green400 } from 'material-ui/styles/colors';
+import { blue300, yellow300, red300, green300 } from 'material-ui/styles/colors';
 
 import axios from 'axios'
-
 
 const styles = {
   wrapper: {
@@ -38,21 +37,20 @@ const filterData = {
   projectStatus: [
     {
       title: '已申請教授',
-      color: green400
+      color: green300
     },
     {
       title: '待教授審核',
-      color: yellow400
+      color: yellow300
 
     },
     {
       title: '未申請教授',
-      color: red400
+      color: red300
     }
-  ]
+  ],
+  program: ['資工A', '資工B', '網多', '資電', '電資學士班', '其他']
 }
-
-
 
 class StudentsItem extends React.Component {
 
@@ -61,7 +59,7 @@ class StudentsItem extends React.Component {
     this.state = {
       students: [],
       project_status_filter: [true, true, true],
-      program_filter: [true, true, true, true]
+      program_filter: [true, true, true, true, true, true]
     }
   }
 
@@ -81,23 +79,51 @@ class StudentsItem extends React.Component {
     })
   }
 
+  toggleProgramFilter = (index) => {
+    const new_program_filter = this.state.program_filter.slice()
+    new_program_filter[index] = !this.state.program_filter[index]
+    this.setState({
+      program_filter: new_program_filter
+    })
+  }
+
+  mapProgramStringToNumber = (program) => {
+    switch (program) {
+      case '資工A':
+        return 0
+      case '資工B':
+        return 1
+      case '網多':
+        return 2
+      case '資電':
+        return 3
+      case '電資學士班':
+        return 4
+      default:
+        return 5
+    }
+  }
+
   filter = (students) => {
-    const { project_status_filter } = this.state
-    const res = students.filter( (student) => project_status_filter[student.project.status] )
-    return res
+    const { project_status_filter, program_filter } = this.state
+    const filtered_students = students.filter( (student) => (
+      project_status_filter[student.project.status] &&
+      program_filter[this.mapProgramStringToNumber(student.student.program)]
+    ))
+    return filtered_students
   }
 
   render() {
-    const { students, project_status_filter } = this.state
+    const { students, project_status_filter, program_filter } = this.state
     return (
       <div style = { styles.wrapper }>
         <div className='row' style={{marginBottom: '50px', marginTop: '20px'}}>
           <div className='col-md-9 col-lg-9 hidden-xs' >
             <Paper style = { styles.paper } zDepth={3}>
               <TextField
-                hintText="學號/姓名/組別"
-                floatingLabelText="搜尋欄位"
-                style = { styles.searchTextField }
+                hintText          = "學號/姓名/組別"
+                floatingLabelText = "搜尋欄位"
+                style             = { styles.searchTextField }
               />
               <StudentsTable students = { this.filter(students) }/>
             </Paper>
@@ -110,10 +136,10 @@ class StudentsItem extends React.Component {
               {filterData.projectStatus.map((item, index) => (
                 <div>
                   <Chip
-                    onClick = { () => this.toggleProjectStatusFilter(index) }
+                    onClick         = { () => this.toggleProjectStatusFilter(index) }
                     backgroundColor = { project_status_filter[index] ? item.color : null }
-                    style = { styles.chip }
-                    labelStyle = { styles.chipText }
+                    style           = { styles.chip }
+                    labelStyle      = { styles.chipText }
                   >
                     { item.title }
                   </Chip>
@@ -124,31 +150,28 @@ class StudentsItem extends React.Component {
               <hr />
               <h3>組別</h3>
               <hr />
-              <div className = 'row'>
-                <div className='col-md-6 col-lg-6 hidden-xs' >
-                  <Chip style = { styles.chip } labelStyle = { styles.chipText } >
-                    資A
-                  </Chip>
-                </div>
-                <div className='col-md-6 col-lg-6 hidden-xs' >
-                  <Chip style = { styles.chip } labelStyle = { styles.chipText } >
-                    資B
-                  </Chip>
-                </div>
-              </div>
-              <br />
-              <div className = 'row'>
-                <div className='col-md-6 col-lg-6 hidden-xs' >
-                  <Chip style = { styles.chip } labelStyle = { styles.chipText } >
-                    網多
-                  </Chip>
-                </div>
-                <div className='col-md-6 col-lg-6 hidden-xs' >
-                  <Chip style = { styles.chip } labelStyle = { styles.chipText } >
-                    資電
-                  </Chip>
-                </div>
-              </div>
+              {
+                [0, 2, 4].map( (beginNumber) => (
+                  <div>
+                    <div className = 'row'>
+                      {
+                        [0, 1].map( (diffNumber) => (
+                          <div className='col-md-6 col-lg-6 hidden-xs' >
+                            <Chip
+                              onClick         = { () => this.toggleProgramFilter(beginNumber + diffNumber) }
+                              backgroundColor = { program_filter[beginNumber + diffNumber] ? blue300 : null }
+                              style           = { styles.chip }
+                              labelStyle      = { styles.chipText }
+                            >
+                              { filterData.program[beginNumber + diffNumber] }
+                            </Chip>
+                          </div>
+                      ))}
+                    </div>
+                    <br />
+                  </div>
+                ))
+              }
             </Paper>
           </div>
         </div>
