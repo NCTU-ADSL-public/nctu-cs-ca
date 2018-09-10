03812990550a6292c68f632671044562ebc9233b
marmot0814 <s1357andy871207@gmail.com>
2018-09-11 00:57:33 +0800

    first_second int to string bug

--- ca/src/Redux/Assistants/Actions/Project_v3/Score.js
+++ ca/src/Redux/Assistants/Actions/Project_v3/Score.js
@@ -21,7 +21,7 @@ export const downloadCsv = req => dispatch => {
     const blob = new Blob([res.data], {type: "text/plain"})
     const link = document.createElement("a")
     link.href = URL.createObjectURL(blob)
-    const filename = req.semester.substring(0, 3) + `學年度` + (req.semester[4] === "1" ? `上` : `下`) + `學期 資工專題(` + ( req.first_second === 1 ? `一` : `二` ) + `)`
+    const filename = req.semester.substring(0, 3) + `學年度` + (req.semester[4] === "1" ? `上` : `下`) + `學期 資工專題(` + ( req.first_second === "1" ? `一` : `二` ) + `)`
     link.download = `${filename}.csv` // 这里填保存成的文件名
     link.click()
     URL.revokeObjectURL(link.href)
