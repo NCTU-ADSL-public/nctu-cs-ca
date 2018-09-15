e31188e10cb6a7cb47bcfc48f0033d86b785920e
marmot0814 <s1357andy871207@gmail.com>
2018-09-16 00:01:16 +0800

    add filter

--- ca/src/Pages/Assistants/Project_v3/Teacher/index.js
+++ ca/src/Pages/Assistants/Project_v3/Teacher/index.js
@@ -1,33 +1,84 @@
 import React from 'react'
 import { withStyles } from '@material-ui/core/styles';
 import { connect } from 'react-redux'
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import SwipeableViews from 'react-swipeable-views';
-import Accepted from './Accepted'
-import Pending from './Pending'
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Chip from '@material-ui/core/Chip';
+import FirstPage from '@material-ui/icons/FirstPage';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import LastPage from '@material-ui/icons/LastPage';
+import grey from '@material-ui/core/colors/grey';
+import blue from '@material-ui/core/colors/blue';
+import red from '@material-ui/core/colors/red';
+import FormControl from '@material-ui/core/FormControl';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import AccessTime from '@material-ui/icons/AccessTime';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Badge from '@material-ui/core/Badge';
+import Tooltip from '@material-ui/core/Tooltip';
+import yellow from '@material-ui/core/colors/yellow';
+import green from '@material-ui/core/colors/green';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Dialog from '@material-ui/core/Dialog';
 
 import { fetchTeachers } from '../../../../Redux/Assistants/Actions/Project_v3/Teacher'
 
+const STATE_COLOR = [ red['A100'], blue[300] ]
 
 const styles = theme => ({
-  root: {
-    width: '60%',
-    margin: '0 auto'
+  chip: {
+    margin: '10px',
+    fontSize: '15px',
   },
-  tabsIndicator: {
-    backgroundColor: 'rgb(0, 188, 212)'
+  icon: {
+    fontSize: '40px',
+    display: 'inline-flex',
+    verticalAlign: 'middle',
+    color: grey[600],
+    '&:hover': {
+      color: grey[900],
+      transition: 'color 0.5s'
+    },
+    transition: 'color 0.3s',
+    marginRight: '10px',
+    marginLeft: '10px'
   },
-  tabRoot: {
-    minWidth: '27.5%',
-    minHeight: '10px'
-  },
-  tabLabel: {
-    fontSize: 18
-  },
-  tabSelected: {
+  iconWithColor: {
+    fontSize: '40px',
+    display: 'inline-flex',
+    verticalAlign: 'middle',
     color: 'rgb(0, 188, 212)',
-    transition: 'color 0.3s'
+    '&:hover': {
+      color: 'rgb(0, 168, 192)',
+      transition: 'color 0.5s'
+    },
+    transition: 'color 0.3s',
+    marginRight: '10px',
+    marginLeft: '10px'
+  },
+  cssLabel: {
+    fontSize: 15,
+    '&$cssFocused': {
+      color: 'rgb(0, 188, 212)'
+    },
+  },
+  cssFocused: {},
+  cssUnderline: {
+    '&:after': {
+      borderBottomColor: 'rgb(0, 188, 212)'
+    },
+  },
+  tooltip: {
+    fontSize: 15
+  },
+  expansionPanelSummaryRootPending: {
+    background: yellow[100]
   }
 })
 
@@ -36,50 +87,224 @@ class index extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      tabIndex: 0,
+      expanded: null,
+      page: 0,
+      number_per_page: 10,
+      input: '',
+      grade: "04",
+      semester: "106-2",
+      onlyPending: false
     }
-    props.fetch_teachers({ grade: "04", semester: "106-2" })
+  }
+
+  filter = (teachers) => {
+    const { input, onlyPending } = this.state
+    return (
+      teachers.filter( (teacher) =>
+        (   input === ''
+         || teacher.professor_name.toLowerCase().search(input.toLowerCase()) !== -1
+        )
+        &&
+        (   !onlyPending
+         || teacher.professor_status === "1"
+        )
+      )
+    )
+  }
+
+  toggleFilter = target => {
+    this.setState({ filter_status: this.state.filter_status.map((value, index) => target === index ? !value : value ), page: 0 })
   }
 
   render() {
 
-    const { classes, teachers } = this.props
-    const { tabIndex } = this.state
+    const { classes, teachers, professor_name, fetch_teachers } = this.props
+    const { expanded, page, number_per_page, input, grade, semester, onlyPending } = this.state
 
     return (
-      <div className = { classes.root } >
-        <Tabs
-          value = { tabIndex }
-          onChange = { (event, value) => this.setState({ tabIndex: value }) }
-          centered
-          classes = {{
-            root: classes.tabsRoot,
-            indicator: classes.tabsIndicator
-          }}
-        >
-          <Tab
-            label = "接受列表"
-            classes = {{
-              root: classes.tabRoot,
-              label: classes.tabLabel,
-              selected: classes.tabSelected
-            }}
-          />
-          <Tab
-            label = "審核列表"
-            classes = {{
-              root: classes.tabRoot,
-              label: classes.tabLabel,
-              selected: classes.tabSelected
-            }}
-          />
-        </Tabs>
-        <SwipeableViews
-          index = { tabIndex }
-        >
-          <div><Accepted teachers = { teachers } /></div>
-          <div><Pending teachers = { teachers } /></div>
-        </SwipeableViews>
+      <div style = {{ marginBottom: '60px', width: '60%', margin: '0 auto', marginTop: '20px' }} >
+        <div className = 'row' >
+          <div className = 'col-md-4 col-lg-4 col-xs-12' style = {{ display: 'flex' }} >
+            <tooltip title = { onlyPending ? '顯示全部' : '僅顯示專題審核'} placement="top" classes = {{ tooltip: classes.tooltip }} >
+              <AccessTime className = { onlyPending ? classes.iconWithColor : classes.icon } onClick = { () => this.setState({ onlyPending: !onlyPending }) } />
+            </tooltip>
+            <FormControl style = {{ width: '100%', marginBottom: '10px', flex: 1 }}>
+              <InputLabel
+                FormLabelClasses={{
+                  root: classes.cssLabel,
+                  focused: classes.cssFocused,
+                }}
+              >
+                搜尋教授 姓名
+              </InputLabel>
+              <Input
+                classes={{
+                  underline: classes.cssUnderline,
+                }}
+                onChange = { (event) => this.setState({ input: event.target.value, page: 0 }) }
+                value = { input }
+              />
+            </FormControl>
+          </div>
+          <div className = 'col-md-4 col-lg-4 col-xs-12' >
+            <FormControl style = {{ width: '100%' }}>
+              <InputLabel
+                FormLabelClasses={{
+                  root: classes.cssLabel,
+                  focused: classes.cssFocused,
+                }}
+              >
+                學期
+              </InputLabel>
+              <Select
+                input = {
+                  <Input
+                    classes={{
+                      underline: classes.cssUnderline,
+                    }}
+                  />
+                }
+                value = { this.state.semester }
+                style = {{ fontSize: '15px' }}
+                onChange={
+                  (event) => {
+                    fetch_teachers({ semester: event.target.value, grade })
+                    this.setState({ semester: event.target.value, page: 0 })
+                  }
+                }
+              >
+                <MenuItem value = { "all" } style = {{ fontSize: '20px' }} >全部學期</MenuItem>
+                <MenuItem value = { "106-1" } style = {{ fontSize: '20px' }} >106上學期</MenuItem>
+                <MenuItem value = { "106-2" } style = {{ fontSize: '20px' }} >106下學期</MenuItem>
+                <MenuItem value = { "107-1" } style = {{ fontSize: '20px' }} >107上學期</MenuItem>
+                <MenuItem value = { "107-2" } style = {{ fontSize: '20px' }} >107下學期</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
+          <div className = 'col-md-4 col-lg-4 col-xs-12' >
+            <FormControl style = {{ width: '100%' }}>
+              <InputLabel
+                FormLabelClasses={{
+                  root: classes.cssLabel,
+                  focused: classes.cssFocused,
+                }}
+              >
+                系級
+              </InputLabel>
+              <Select
+                input = {
+                  <Input
+                    classes={{
+                      underline: classes.cssUnderline,
+                    }}
+                  />
+                }
+                value = { this.state.grade }
+                style = {{ fontSize: '15px' }}
+                onChange={
+                  (event) => {
+                    fetch_teachers({ grade: event.target.value, semester })
+                    this.setState({ grade: event.target.value, page: 0 })
+                  }
+                }
+              >
+                {[...Array(9)].map((x, i) => <MenuItem value = { "0" + (i + 1) } style = {{ fontSize: '20px' }} >{"0" + (i + 1)}</MenuItem>)}
+              </Select>
+            </FormControl>
+          </div>
+        </div>
+        <div style = {{ minHeight: '570px' }} >
+          {this.filter(teachers).slice(page * number_per_page, (page + 1) * number_per_page)
+          .map( ( teacher, index ) => {
+            return (
+              <div style = {{ margin: '5px auto', width: '100%' }}>
+                <ExpansionPanel expanded = { expanded === index } onChange = { () => this.setState({ expanded: expanded === index ? null : index }) } >
+                  <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />} classes = {{ root: teacher.professor_status === "1" ? classes.expansionPanelSummaryRootPending : classes.expansionPanelSummaryRoot }} >
+                    <div style = {{ width: '100%', display: 'flex' }} >
+                      <div style = {{ fontSize: 20, flex: 0.2, textAlign: 'center', color: 'black' }} >{ teacher.professor_name }</div>
+                      <LinearProgress variant="determinate"
+                        value = { teacher.gradeCnt / 7 * 100 }
+                        style = {{ flex: 0.6, margin: '10px auto' }}
+                      />
+                      <div style = {{ fontSize: 20, flex: 0.2, textAlign: 'center', color: 'black'  }} >{ teacher.gradeCnt } 人</div>
+                    </div>
+                  </ExpansionPanelSummary>
+                  <ExpansionPanelDetails>
+                  <div style = {{ width: '100%' }}>
+                    <div style = {{ fontWeight: 'bold', fontSize: '30px' }}>接受列表</div>
+                    {
+                      teacher.accepted.projects.length !== 0 ?
+                        <div style = {{ display: 'block' }}>
+                        {
+                          teacher.accepted.projects.map( project => {
+                            return (
+                              <div style = {{ marginBottom: '40px' }} >
+                                <hr />
+                                <div style = {{ fontSize: 20, color: 'black', fontWeight: 'bold' }} >{ project.title }</div>
+                                <br />
+                                {
+                                  project.students.map( student => (
+                                    <Tooltip title = { (student.first_second === "1" ? "專題一" : "專題二") + " " +  project.students[0].semester.substr(0, 3) + (project.students[0].semester[4] === "1" ? "上" : "下")  } placement="top" classes = {{ tooltip: classes.tooltip }} >
+                                      <Chip label = { student.id + " " + student.name } className = { classes.chip } style = {{ background: STATE_COLOR[(student.status === "0" ? 0 : 1)] }} />
+                                    </Tooltip>
+                                  ))
+                                }
+                              </div>
+                            )
+                          })
+                        }
+                        </div>
+                        :
+                        <div style = {{ width: '100%', display: 'flex', justifyContent: "center" }}>
+                          <div style = {{ fontSize: 18, color: grey[500] }} >尚無接受專題</div>
+                        </div>
+                    }
+                    <div style = {{ fontWeight: 'bold', fontSize: '30px' }}>審核列表</div>
+                    {
+                      teacher.pending.projects.length !== 0 ?
+                      <div style = {{ display: 'block' }}>
+                      {
+                        teacher.pending.projects.map( project => {
+                          return (
+                            <div style = {{ marginBottom: '40px' }} >
+                            <hr />
+                            <div style = {{ fontSize: 20, color: 'black', fontWeight: 'bold' }} >{ project.title }</div>
+                            <br />
+                            {
+                              project.students.map( student => (
+                                <Chip label = { student.id + " " + student.name } className = { classes.chip } style = {{ background: STATE_COLOR[(student.state === "0" ? 0 : 1)] }} />
+                              ))
+                            }
+                            </div>
+                          )
+                        })
+                      }
+                      </div>
+                      :
+                      <div style = {{ width: '100%', display: 'flex', justifyContent: "center"}}>
+                      <div style = {{ fontSize: 18, color: grey[500] }} >尚未有申請的專題</div>
+                      </div>
+                    }
+                    </div>
+                  </ExpansionPanelDetails>
+                </ExpansionPanel>
+              </div>
+            )
+          })}
+        </div>
+        <div style = {{ textAlign: 'center', marginTop: '10px' }} >
+          <FirstPage className = { classes.icon } onClick = { () => this.setState({ page: 0 }) } />
+          <ChevronLeft className = { classes.icon } onClick = { () => this.setState({ page: Math.max(0, page - 1) }) } />
+          <span style = {{
+            display: 'inline-flex',
+            verticalAlign: 'middle',
+            fontSize: '20px',
+            marginRight: '20px',
+            marginLeft: '20px'
+          }}>{page + 1} / { Math.max(1, Math.ceil(this.filter(teachers).length / number_per_page)) }</span>
+          <ChevronRight className = { classes.icon } onClick = { () => this.setState({ page: Math.min(Math.ceil(this.filter(teachers).length / number_per_page) - 1, page + 1) }) } />
+          <LastPage className = { classes.icon } onClick = { () => this.setState({ page: Math.ceil(this.filter(teachers).length / number_per_page) - 1 }) } />
+        </div>
       </div>
     )
   }
--- ca/src/Redux/Assistants/Reducers/Project_v3/Teacher.js
+++ ca/src/Redux/Assistants/Reducers/Project_v3/Teacher.js
@@ -1,8 +1,180 @@
 import { handleActions } from 'redux-actions'
 
 const initialState = {
-  teachers: []
+  teachers: [{
+    "professor_name": "易志偉",
+    "professor_status":"1",
+    "gradeCnt": 5,
+    "accepted": {
+        "projects": [
+            {
+                "title": "Train Machines by Machines",
+                "students": [
+                    {
+                        "id": "0416054",
+                        "name": "莊紹平",
+                        "program": "資工系 資工組3B ",
+                        "semester": "107-1",
+                        "project_status":"0",
+                        "first_second": "2",
+                        "status": "0"
+
+                    }
+                ]
+            },
+            {
+                "title": "自動化深度學習伺服器佈署",
+                "students": [
+                    {
+                        "id": "0416096",
+                        "name": "徐浚于",
+                        "program": "資工系資工組3B",
+                        "semester": "106-2",
+                        "project_status":"0",
+                        "first_second": "2",
+                        "status": "1"
+                    },
+                    {
+                        "id": "0416235",
+                        "name": "劉昱劭",
+                        "program": "資工系網多組3",
+                        "semester": "106-2",
+                        "project_status":"0",
+                        "first_second": "2",
+                        "status": "1"
+                    }
+                ]
+            },
+            {
+                "title": "深度學習熱度圖用於姿態判斷",
+                "students": [
+                    {
+                        "id": "0416102",
+                        "name": "鄭子琳",
+                        "program": "資工系 資工組3B ",
+                        "semester": "107-1",
+                        "project_status":"0",
+                        "first_second": "2",
+                        "status": "0"
+                    },
+                    {
+                        "id": "0416104",
+                        "name": "李佩琪",
+                        "program": "資工系資工組3B",
+                        "semester": "107-1",
+                        "project_status":"0",
+                        "first_second": "2",
+                        "status": "0"
+                    }
+                ]
+            }
+        ]
+    },
+    "pending": {
+        "projects": [
+            {
+                "title": "摩托車",
+                "students": [
+                    {
+                        "id": "0416202",
+                        "name": "黃柏閎",
+                        "program": "網多",
+                        "first_second": "1",
+                        "status": "1"
+                    }
+                ]
+            }
+        ]
+    }
+},
+{
+  "professor_name": "易志偉",
+  "professor_status":"0",
+  "gradeCnt": 5,
+  "accepted": {
+      "projects": [
+          {
+              "title": "Train Machines by Machines",
+              "students": [
+                  {
+                      "id": "0416054",
+                      "name": "莊紹平",
+                      "program": "資工系 資工組3B ",
+                      "semester": "107-1",
+                      "project_status":"0",
+                      "first_second": "2",
+                      "status": "0"
+
+                  }
+              ]
+          },
+          {
+              "title": "自動化深度學習伺服器佈署",
+              "students": [
+                  {
+                      "id": "0416096",
+                      "name": "徐浚于",
+                      "program": "資工系資工組3B",
+                      "semester": "106-2",
+                      "project_status":"0",
+                      "first_second": "2",
+                      "status": "1"
+                  },
+                  {
+                      "id": "0416235",
+                      "name": "劉昱劭",
+                      "program": "資工系網多組3",
+                      "semester": "106-2",
+                      "project_status":"0",
+                      "first_second": "2",
+                      "status": "1"
+                  }
+              ]
+          },
+          {
+              "title": "深度學習熱度圖用於姿態判斷",
+              "students": [
+                  {
+                      "id": "0416102",
+                      "name": "鄭子琳",
+                      "program": "資工系 資工組3B ",
+                      "semester": "107-1",
+                      "project_status":"0",
+                      "first_second": "2",
+                      "status": "0"
+                  },
+                  {
+                      "id": "0416104",
+                      "name": "李佩琪",
+                      "program": "資工系資工組3B",
+                      "semester": "107-1",
+                      "project_status":"0",
+                      "first_second": "2",
+                      "status": "0"
+                  }
+              ]
+          }
+      ]
+  },
+  "pending": {
+      "projects": [
+          {
+              "title": "摩托車",
+              "students": [
+                  {
+                      "id": "0416202",
+                      "name": "黃柏閎",
+                      "program": "網多",
+                      "first_second": "1",
+                      "status": "1"
+                  }
+              ]
+          }
+      ]
+  }
+}]
 }
+
 export default handleActions({
   STORE_TEACHERS: (state, action) => ({ ...state,
     teachers: action.payload
