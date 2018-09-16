ea2c63361b8ae6016dd652841ee035aa5693c2a7
marmot0814 <s1357andy871207@gmail.com>
2018-09-17 01:07:38 +0800

    test api

--- ca/src/Redux/Assistants/Reducers/Project_v3/Teacher.js
+++ ca/src/Redux/Assistants/Reducers/Project_v3/Teacher.js
@@ -93,19 +93,15 @@ export default handleActions({
   STORE_TEACHERS: (state, action) => ({ ...state,
     teachers: action.payload
   }),
-  UPDATE_ADD_STATUS: (state, action) => {
-    const newTeachers = state.teachers.map( teacher => (
-      teacher.accepted.projects.map( project => (
-        project.students.map( student => ({...student,
+  UPDATE_ADD_STATUS: (state, action) => ({ ...state,
+    teachers: state.teachers.map( teacher => ({ ...teacher,
+      accepted: { ...teacher.accepted,
+        projects: teacher.accepted.projects.map( project => ({ ...project,
+          students: project.students.map( student => ({...student,
             add_status: student.id === action.payload ? "1" : student.add_status
-          })
-        )
-      ))
-    ))
-    console.log("NEWTEACHERS")
-    console.log(newTeachers)
-    return ({ ...state,
-      teachers: newTeachers
-    })
-  }
+          }))
+        }))
+      }
+    }))
+  })
 }, initialState)
