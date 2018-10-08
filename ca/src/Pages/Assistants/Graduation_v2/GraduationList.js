d1c8550ab125ced3d5616c7046214426702e8281
Marmot Yang <s1357andy871207@gmail.com>
2018-10-08 21:52:58 +0800

    add filter and input

--- ca/src/Pages/Assistants/Graduation_v2/GraduationList.js
+++ ca/src/Pages/Assistants/Graduation_v2/GraduationList.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux'
 
 
 
+import TableFooter from '@material-ui/core/TableFooter';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -11,11 +12,16 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import CircularProgressbar from 'react-circular-progressbar'
 import TablePagination from '@material-ui/core/TablePagination';
+import FirstPage from '@material-ui/icons/FirstPage';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import LastPage from '@material-ui/icons/LastPage';
+import grey from '@material-ui/core/colors/grey';
+import green from '@material-ui/core/colors/green';
+import blue from '@material-ui/core/colors/blue';
+import red from '@material-ui/core/colors/red';
 
 const styles = theme => ({
-  table: {
-    marginTop: '50px',
-  },
   font:{
     fontSize:20,
     fontWeight:400,
@@ -23,10 +29,23 @@ const styles = theme => ({
     justifyContent: 'center',
     textAlign: 'center'
   },
+  icon: {
+    fontSize: '40px',
+    display: 'inline-flex',
+    verticalAlign: 'middle',
+    color: grey[600],
+    '&:hover': {
+      color: grey[900],
+      transition: 'color 0.5s'
+    },
+    transition: 'color 0.3s',
+    marginRight: '10px',
+    marginLeft: '10px'
+  },
 })
 
 const mapStateToProps = (state) => ({
-  students: state.Assistant.Graduation.students
+
 })
 
 const mapDispatchToProps = (dispatch) => ({
@@ -34,54 +53,93 @@ const mapDispatchToProps = (dispatch) => ({
 })
 
 const GRAD_STATUS_CN = ['未達標', '將達標', '已達標']
-const GRAD_STATUS_COLOR = ['red', 'blue', 'green']
+const GRAD_STATUS_COLOR = [red[600], blue[600], green[600]]
 
 class index extends React.Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      studentsPerPage: 10,
+      page: 0
+    }
+  }
+
+  handleChangePage = (event, page) => {
+    this.setState({ page });
+  };
+
+  handleChangeRowsPerPage = event => {
+    this.setState({ studentsPerPage: event.target.value });
+  };
 
   render() {
 
     const { classes, students } = this.props
+    const { studentsPerPage, page } = this.state
+    const emptyRows = studentsPerPage - Math.min(studentsPerPage, students.length - page * studentsPerPage);
 
     return (
-      <Table className={classes.table}>
-        <TableHead>
-          <TableRow>
+      <div>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              {
+                ['學號', '姓名', '組別', '總學分', '畢業狀態'].map( title =>
+                  <TableCell className={classes.font}>{ title }</TableCell>
+                )
+              }
+            </TableRow>
+          </TableHead>
+          <TableBody>
             {
-              ['學號', '姓名', '組別', '總學分', '畢業狀態'].map( title =>
-                <TableCell className={classes.font}>{ title }</TableCell>
-              )
+              students
+              .slice(page * studentsPerPage, page * studentsPerPage + studentsPerPage)
+              .map ( student => (
+                <TableRow onClick={()=>console.log("Hello")}>
+                  <TableCell className={classes.font}>{student.id}</TableCell>
+                  <TableCell className={classes.font}>{student.name}</TableCell>
+                  <TableCell className={classes.font}>{student.program}</TableCell>
+                  <TableCell className={classes.font}>
+                    <div style={{ width: "50px", margin: '0 auto', marginTop: '5px' }}>
+                      <CircularProgressbar
+                        percentage={100 * student.total / 128}
+                        text={student.total}
+                        initialAnimation
+                        styles={{
+                          path: { stroke: '#34855e' },
+                          text: { fill: '#34855e', fontSize: '30px', fontWeight: 'bold' }
+                        }}
+                      />
+                    </div>
+                  </TableCell>
+                  <TableCell className={classes.font} style = {{ color: GRAD_STATUS_COLOR[parseInt(student.status)]}}>
+                    {GRAD_STATUS_CN[parseInt(student.status)]}
+                  </TableCell>
+                </TableRow>
+              ))
             }
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {
-            students.map ( student => (
-              <TableRow>
-                <TableCell className={classes.font}>{student.id}</TableCell>
-                <TableCell className={classes.font}>{student.name}</TableCell>
-                <TableCell className={classes.font}>{student.program}</TableCell>
-                <TableCell className={classes.font}>
-                  <div style={{ width: "50px", margin: '0 auto', marginTop: '5px' }}>
-                    <CircularProgressbar
-                      percentage={100 * student.total / 128}
-                      text={student.total}
-                      initialAnimation
-                      styles={{
-                        path: { stroke: '#34855e' },
-                        text: { fill: '#34855e', fontSize: '30px', fontWeight: 'bold' }
-                      }}
-                    />
-                  </div>
-                </TableCell>
-                <TableCell className={classes.font} style = {{ color: GRAD_STATUS_COLOR[parseInt(student.status)]}}>
-                  {GRAD_STATUS_CN[parseInt(student.status)]}
-                </TableCell>
+            {emptyRows > 0 && (
+              <TableRow style={{ height: 71 * emptyRows }}>
+                <TableCell colSpan={6} />
               </TableRow>
-            ))
-          }
-        </TableBody>
-      </Table>
+            )}
+          </TableBody>
+        </Table>
+        <div style = {{ textAlign: 'center', marginTop: '10px', marginBottom: '50px' }} >
+          <FirstPage className = { classes.icon } onClick = { () => this.setState({ page: 0 }) } />
+          <ChevronLeft className = { classes.icon } onClick = { () => this.setState({ page: Math.max(0, page - 1) }) } />
+          <span style = {{
+            display: 'inline-flex',
+            verticalAlign: 'middle',
+            fontSize: '20px',
+            marginRight: '20px',
+            marginLeft: '20px'
+          }}>{page + 1} / { Math.max(1, Math.ceil(students.length / studentsPerPage)) }</span>
+          <ChevronRight className = { classes.icon } onClick = { () => this.setState({ page: Math.max(0, Math.min(Math.ceil(students.length / studentsPerPage) - 1, page + 1)) }) } />
+          <LastPage className = { classes.icon } onClick = { () => this.setState({ page: Math.max(0, Math.ceil(students.length / studentsPerPage) - 1) }) } />
+        </div>
+      </div>
     )
   }
 }
--- ca/src/Pages/Assistants/Graduation_v2/index.js
+++ ca/src/Pages/Assistants/Graduation_v2/index.js
@@ -3,16 +3,63 @@ import { withStyles } from '@material-ui/core/styles';
 import { connect } from 'react-redux'
 
 import GraduationList from './GraduationList'
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Dialog from '@material-ui/core/Dialog';
+import Chip from '@material-ui/core/Chip';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import FilterList from '@material-ui/icons/FilterList';
+import Done from '@material-ui/icons/Done';
+import Clear from '@material-ui/icons/Clear';
+import QueryBuilder from '@material-ui/icons/QueryBuilder';
+import FormControl from '@material-ui/core/FormControl';
+import grey from '@material-ui/core/colors/grey';
+import green from '@material-ui/core/colors/green';
+import blue from '@material-ui/core/colors/blue';
+import red from '@material-ui/core/colors/red';
 
 const styles = theme => ({
   container: {
     width: '70%',
-    margin: '0 auto'
-  }
+    margin: '0 auto',
+    marginBottom: '30px'
+  },
+  cssLabel: {
+    fontSize: 20,
+    '&$cssFocused': {
+      color: 'rgb(0, 188, 212)'
+    },
+  },
+  cssFocused: {},
+  cssUnderline: {
+    '&:after': {
+      borderBottomColor: 'rgb(0, 188, 212)'
+    },
+  },
+  chip: {
+    margin: '10px',
+    marginTop: '20px',
+    marginBottom: '20px',
+    fontSize: '20px',
+    padding: '10px'
+  },
+  icon: {
+    fontSize: '40px',
+    display: 'inline-flex',
+    verticalAlign: 'middle',
+    color: grey[600],
+    '&:hover': {
+      color: grey[900],
+      transition: 'color 0.5s'
+    },
+    transition: 'color 0.3s',
+    marginRight: '10px',
+    marginLeft: '10px'
+  },
 })
 
 const mapStateToProps = (state) => ({
-
+  students: state.Assistant.Graduation.students
 })
 
 const mapDispatchToProps = (dispatch) => ({
@@ -21,13 +68,106 @@ const mapDispatchToProps = (dispatch) => ({
 
 class index extends React.Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      filter_status: [false, false, false],
+      input: '',
+      open_filter: false
+    }
+  }
+
+  filter = (students) => {
+    const { input, filter_status } = this.state
+    return (
+      students.filter( (student) =>
+        (    input === ''
+          || student.name.toLowerCase().search(input.toLowerCase()) !== -1
+          || student.id.search(input) !== -1
+        )
+        &&
+        (
+             !filter_status.reduce( (haveTrue, value) => haveTrue || value, false)
+          || filter_status[parseInt(student.status)]
+        )
+      )
+    )
+  }
+
+  toggleFilter = target => {
+    this.setState({ filter_status: this.state.filter_status.map((value, index) => target === index ? !value : value ), page: 0 })
+  }
+
   render() {
 
-    const { classes } = this.props
+    const { classes, students } = this.props
+    const { filter_status, open_filter, input } = this.state
+
+
 
     return (
       <div className = { classes.container } >
-        <GraduationList />
+        <div className = 'row' style = {{ marginTop: '30px', marginBottom: '20px' }}>
+          <div className = 'col-md-6 col-lg-6 col-xs-12' style = {{ display: 'flex' }} >
+            <FilterList className = { classes.icon } onClick = { () => this.setState({ open_filter: true }) } />
+            <Dialog onClose = { () => this.setState({ open_filter: false })} open = { open_filter } >
+              <DialogTitle><div style = {{ fontSize: '30px' }} >畢業預審狀況</div></DialogTitle>
+              <div style = {{ display: 'flex' }}>
+                <Chip
+                  label = {
+                    <span>
+                      <Clear style = {{ fontSize: '30px', verticalAlign: 'middle', marginRight: '5px' }} />
+                      <div style = {{ display: 'inline', verticalAlign: 'middle' }} >未達標</div>
+                    </span>
+                  }
+                  className = { classes.chip }
+                  onClick = { () => this.toggleFilter(0) }
+                  style = {{ background: filter_status[0] ? red[300] : null }}
+                />
+                <Chip
+                  label = {
+                    <span>
+                      <QueryBuilder style = {{ fontSize: '30px', verticalAlign: 'middle', marginRight: '5px' }} />
+                      <div style = {{ display: 'inline', verticalAlign: 'middle' }} >將達標</div>
+                    </span>
+                  }
+                  className = { classes.chip }
+                  onClick = { () => this.toggleFilter(1) }
+                  style = {{ background: filter_status[1] ? blue[300] : null }}
+                />
+                <Chip
+                  label = {
+                    <span>
+                      <Done style = {{ fontSize: '30px', verticalAlign: 'middle', marginRight: '5px' }} />
+                      <div style = {{ display: 'inline', verticalAlign: 'middle' }} >已達標</div>
+                    </span>
+                  }
+                  className = { classes.chip }
+                  onClick = { () => this.toggleFilter(2) }
+                  style = {{ background: filter_status[2] ? green[300] : null }}
+                />
+              </div>
+            </Dialog>
+            <FormControl style = {{ width: '100%', flex: 1 }} >
+              <InputLabel
+                FormLabelClasses={{
+                  root: classes.cssLabel,
+                  focused: classes.cssFocused,
+                }}
+              >
+                搜尋學生 姓名 / 學號
+              </InputLabel>
+              <Input
+                classes={{
+                  underline: classes.cssUnderline,
+                }}
+                onChange = { (event) => this.setState({ input: event.target.value, page: 0 }) }
+                value = { input }
+              />
+            </FormControl>
+          </div>
+        </div>
+        <GraduationList students = { this.filter(students) }/>
       </div>
     )
   }
