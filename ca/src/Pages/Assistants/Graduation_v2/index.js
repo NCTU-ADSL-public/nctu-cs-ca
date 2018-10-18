66de9bb0eb426a99ab0f46f7c055380962b93c63
Marmot Yang <s1357andy871207@gmail.com>
2018-10-18 09:52:16 +0800

    test api

--- ca/src/Pages/Assistants/Graduation_v2/index.js
+++ ca/src/Pages/Assistants/Graduation_v2/index.js
@@ -84,7 +84,7 @@ class index extends React.Component {
       open_filter: false,
       page: 0,
       studentsPerPage: 8,
-      grade: '四'
+      grade: '一'
     }
     props.fetch_graduate_list( {grade: this.state.grade} )
     console.log(props.students)
