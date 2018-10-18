cba1345c7acb52fa8885f7d3eb0d80e9ad1a33c5
Marmot Yang <s1357andy871207@gmail.com>
2018-10-18 12:14:48 +0800

    test api

--- ca/src/Redux/Assistants/Actions/Graduation_v2/index.js
+++ ca/src/Redux/Assistants/Actions/Graduation_v2/index.js
@@ -11,7 +11,7 @@ export const fetchGraduateList = (post_item) => dispatch => {
     console.log("RES DATA")
     console.log(res.data)
     // res.data.map( (student, index) => dispatch(fetchDetail( { student_id: student.student_id } )))
-    dispatch(fetchDetail( { student_id: res.data[0].student_id }  ))
+    dispatch(fetchDetail(/*res.data[0].student_id)*/'0616220'))
   }).catch( err => {
     console.log(err)
     console.log("POST_Item")
@@ -19,17 +19,18 @@ export const fetchGraduateList = (post_item) => dispatch => {
   })
 }
 
-export const fetchDetail = post_item => dispatch => {
-  axios.post('/assistants/graduate/glist', post_item).then( res => {
+export const fetchDetail = student_id => dispatch => {
+  axios.get('/assistants/graduate/glist', {
+    student_id
+  }).then( res => {
     console.log("Fetching")
-    console.log("POST_Item")
-    console.log(post_item)
+    console.log("GET_id")
+    console.log(student_id)
     console.log("RES DATA")
     console.log(res.data)
     dispatch(store_graduate_detail(res.data))
   }).catch( err => {
     console.log(err)
     console.log("POST_Item")
-    console.log(post_item)
   })
 }
