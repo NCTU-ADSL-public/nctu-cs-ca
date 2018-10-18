5b23466593a7a11556db69ee5ded80db58cd672b
Marmot Yang <s1357andy871207@gmail.com>
2018-10-18 17:48:36 +0800

    test api

--- ca/src/Redux/Assistants/Reducers/Graduation_v2/index.js
+++ ca/src/Redux/Assistants/Reducers/Graduation_v2/index.js
@@ -1,7 +1,46 @@
 import { handleActions } from 'redux-actions'
 
 const initialState = {
-  students: []
+  students: [
+    {
+      "id": "0416001",
+      "name": "王大名",
+      "program": "資A",
+      "detail": {
+          "total": 116,
+          "status": 0,
+          "compulse":["計算機組織", "資工專題(二)"],
+          "pro": 3,
+          "other": 2,
+          "general": {
+              "old": {
+                  "total": 0,
+                  "contemp": 0,
+                  "culture": 0,
+                  "history": 0,
+                  "citizen": 0,
+                  "group": 0,
+                  "science": 0,
+              },
+              "new": {
+                  "total": 0,
+                  "core": 0,
+                  "basic": 0,
+                  "cross": 0
+              }
+          },
+          "lang": {
+              "status": 0,
+              "basic": 0,
+              "advanced": 0
+          },
+          "pe": 0,
+          "service": 0,
+          "art": 0,
+          "mentor": 0,
+      }
+  }
+  ]
 }
 export default handleActions({
   STORE_GRADUATE_DETAIL: (state, action) => ({ ...state,
