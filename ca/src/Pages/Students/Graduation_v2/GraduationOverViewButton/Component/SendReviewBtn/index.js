271556291bb41960c213197c61b7452b06f63006
Chih-Yu <lcd010308@gmail.com>
2018-10-21 07:43:48 +0800

    畢業送審 (#14)
    * 新增送出預審按鈕
    
    * 修正畢業年度
    
    * 修改假資料
    
    * 修正通識新舊制欄位
    
    * 合併課程資料和列印資料
    
    * 修改送審畫面樣式
    
    * 新增送出按鈕

---
+++ ca/src/Pages/Students/Graduation_v2/GraduationOverViewButton/Component/SendReviewBtn/index.js
@@ -0,0 +1,165 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import MenuItem from '@material-ui/core/MenuItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import CheckIcon from '@material-ui/icons/CheckBox'
+import Button from '@material-ui/core/Button'
+import Dialog from '@material-ui/core/Dialog'
+import DialogContent from '@material-ui/core/DialogContent'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import IconButton from '@material-ui/core/IconButton'
+import CloseIcon from '@material-ui/icons/Close'
+import FormControl from '@material-ui/core/FormControl'
+import Select from '@material-ui/core/Select'
+import InputLabel from '@material-ui/core/InputLabel'
+import withMobileDialog from '@material-ui/core/withMobileDialog/index'
+import PrintForm from '../PrintBtn/PrintForm'
+import { reviewSubmit } from '../../../../../../Redux/Students/Actions/Graduation/'
+
+const styles = theme => ({
+  appBar: {
+    position: 'relative',
+    background: '#3db586'
+  },
+  flex: {
+    flex: 1
+  },
+  select: {
+    fontSize: 20
+  },
+  button: {
+    margin: '20px 0 20px 0',
+    color: '#ffffff'
+  },
+  icon: {
+    marginRight: theme.spacing.unit
+  }
+})
+
+class Index extends React.Component {
+  constructor (props) {
+    super(props)
+    this.handleOpen = this.handleOpen.bind(this)
+    this.handleClose = this.handleClose.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.state = {
+      open: false,
+      generalCourse: {
+        type: 0
+      }
+    }
+  }
+
+  handleOpen () {
+    this.setState({
+      open: true
+    })
+  }
+
+  handleClose () {
+    this.setState({
+      open: false
+    })
+  }
+
+  handleChange (e) {
+    this.setState({
+      generalCourse: {
+        type: e.target.value
+      }
+    })
+  }
+
+  handleSubmit () {
+    this.props.reviewSubmit({
+      general_course: {
+        type: this.state.generalCourse.type
+      }
+    })
+    this.handleClose()
+  }
+
+  render () {
+    const { classes } = this.props
+    console.log(this.state.generalCourse.type)
+    return (
+      <div>
+        <MenuItem className={classes.menuItem} onClick={this.handleOpen}>
+          <ListItemIcon className={classes.icon}>
+            <CheckIcon className={classes.icon} />
+          </ListItemIcon>
+          <ListItemText classes={{ primary: classes.primary }} inset primary='送出預審' />
+        </MenuItem>
+        <Dialog
+          open={this.state.open}
+          onClose={this.handleClose}
+          aria-labelledby='alert-dialog-title'
+          aria-describedby='alert-dialog-description'
+          maxWidth='md'
+        >
+          <AppBar className={classes.appBar}>
+            <Toolbar >
+              <Typography variant='title' color='inherit' className={classes.flex} style={{ fontSize: '15px' }} >
+                確認審查資料
+              </Typography>
+              <IconButton color='inherit' onClick={this.handleClose} aria-label='Close'>
+                <CloseIcon />
+              </IconButton>
+            </Toolbar>
+          </AppBar>
+          <br />
+          <DialogContent>
+            <PrintForm
+              profile={this.props.studentIdcard}
+              graduationCheckEnglishTest={this.props.englishCheck}
+              courseCategoryArray={this.props.reviewData}
+            />
+            <form className='text-center' style={{ marginTop: '30px', fontSize: '20px' }}>                           
+              <label style={{ marginRight: '10px' }}>請選擇採用的通識制度</label>
+              <Select
+                native
+                className={classes.select}
+                value={this.state.generalCourse.type}
+                onChange={this.handleChange}
+              >
+                <option value={0}>舊制</option>
+                <option value={1}>新制</option>
+              </Select>
+              <br />
+              <Button 
+                variant='contained' 
+                color='primary' 
+                className={classes.button}
+                onClick={this.handleSubmit}
+              >
+                確認送出
+              </Button>
+            </form>
+          </DialogContent>
+        </Dialog>
+      </div>
+    )
+  }
+}
+
+Index.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => ({
+  reviewData: state.Student.Graduation.data,
+  studentIdcard: state.Student.User.studentIdcard,
+  englishCheck: state.Student.Graduation.englishCheck
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  reviewSubmit: (payload) => dispatch(reviewSubmit(payload))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(withMobileDialog()(withStyles(styles)(Index)))
