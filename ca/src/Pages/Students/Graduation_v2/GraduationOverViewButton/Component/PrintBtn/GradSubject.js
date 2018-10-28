1ec3a4e1d8174acc9e15debecca8552609bde4e0
Chih-Yu <lcd010308@gmail.com>
2018-10-28 18:58:45 +0800

    Little Refactor in Gradsubject

--- ca/src/Pages/Students/Graduation_v2/GraduationOverViewButton/Component/PrintBtn/GradSubject.js
+++ ca/src/Pages/Students/Graduation_v2/GraduationOverViewButton/Component/PrintBtn/GradSubject.js
@@ -2,38 +2,23 @@ import React from 'react'
 import './GradTable.css'
 
 class GradSubject extends React.Component {
-  constructor (props) {
-    super(props)
-    this.realscore = [null, null, null, null, null, null, null, null]
-    this.state = {
-      name: this.props.name,
-      credit: this.props.credit,
-      realCredit: this.props.realCredit,
-      semester: this.props.semester,
-      year: this.props.year,
-      comment: this.props.comment
-    }
-  }
-
-  componentWillMount () {
-    this.realscore[this.state.year * 2 + this.state.semester - 3] = this.props.score
-  }
-
   render () {
+    let realscore = [null, null, null, null, null, null, null, null]
+    realscore[this.props.year * 2 + this.props.semester - 3] = this.props.score
     return (
-      <tr className={(this.state.name === ' ') ? 'bigger-row' : ''}>
-        <td className='left-text'>{this.state.name}</td>
-        <td>{this.realscore[0]}</td>
-        <td>{this.realscore[1]}</td>
-        <td>{this.realscore[2]}</td>
-        <td>{this.realscore[3]}</td>
-        <td>{this.realscore[4]}</td>
-        <td>{this.realscore[5]}</td>
-        <td>{this.realscore[6]}</td>
-        <td>{this.realscore[7]}</td>
-        <td>{this.state.credit}</td>
-        <td>{this.state.realCredit}</td>
-        <td className='left-text'>{this.state.comment}</td>
+      <tr className={(this.props.name === ' ') ? 'bigger-row' : ''}>
+        <td className='left-text'>{this.props.name}</td>
+        <td>{realscore[0]}</td>
+        <td>{realscore[1]}</td>
+        <td>{realscore[2]}</td>
+        <td>{realscore[3]}</td>
+        <td>{realscore[4]}</td>
+        <td>{realscore[5]}</td>
+        <td>{realscore[6]}</td>
+        <td>{realscore[7]}</td>
+        <td>{this.props.credit}</td>
+        <td>{this.props.realCredit}</td>
+        <td className='left-text'>{this.props.comment}</td>
       </tr>
     )
   }
