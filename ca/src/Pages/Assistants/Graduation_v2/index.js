05aa1720191de9acb31b13151a5b43c119a1ed26
danny <danny021406@gmail.com>
2018-11-05 02:05:04 +0800

    大錯誤辣槓

--- ca/src/Pages/Assistants/Graduation_v2/index.js
+++ ca/src/Pages/Assistants/Graduation_v2/index.js
@@ -123,6 +123,22 @@ class index extends React.Component {
     return ret;
   }
 
+  DownLoadBtn = () => {
+    const { classes, students_csv_data } = this.props
+    if(students_csv_data.length < 1)
+      return ''
+    return(
+      <CSVLink
+        filename={this.state.grade + "年級畢業預審總表.csv"}
+        data={students_csv_data}>
+        <Button variant="contained" className={classes.button} >
+          <CloudDownloadIcon style = {{ fontSize: '20px' }}/>
+          下載
+        </Button>
+      </CSVLink>
+    )
+  }
+
   filter = (students) => {
     const { input, grad_filter_status, veri_filter_status, program_filter_status } = this.state
     return (
@@ -189,7 +205,6 @@ class index extends React.Component {
     const { grad_filter_status, open_filter, input, page, studentsPerPage, grade, veri_filter_status, program_filter_status } = this.state
 
 
-
     return (
       <div className = { classes.container } >
         <div className = 'row' style = {{ marginTop: '30px', marginBottom: '20px' }}>
@@ -201,14 +216,7 @@ class index extends React.Component {
           }
           </div>
           <div className = 'col-md-1 col-lg-1'>
-            <Button variant="contained" className={classes.button} >
-              <CloudDownloadIcon style = {{ fontSize: '20px' }}/>
-              <CSVLink
-                filename={this.state.grade + "年級畢業預審總表.csv"}
-                data={this.props.students_csv_data}>
-                下載
-              </CSVLink>
-            </Button>
+            {this.DownLoadBtn()}
           </div>
           <div className = 'col-md-6 col-lg-6 col-xs-12' style = {{ display: 'flex' }} >
             <FilterList className = { classes.icon } onClick = { () => this.setState({ open_filter: true }) } />
--- ca/src/Redux/Assistants/Actions/Graduation_v2/index.js
+++ ca/src/Redux/Assistants/Actions/Graduation_v2/index.js
@@ -1,4 +1,5 @@
 import { createAction } from 'redux-actions'
+import data_json from './Json'
 import axios from 'axios'
 
 export const set_graduate_state = createAction('SET_GRADUATE_STATE')
@@ -109,6 +110,92 @@ export const fetchStudent = grade => dispatch => {
     }
     dispatch(store_student_csv_data(csvArr))
   })
+  // let data = data_json
+  // let csvArr = []
+  // csvArr.push([
+  //   '學號',
+  //   '姓名',
+  //   '組別',
+  //   '總學分',
+  //   '有沒有英文授課專業課程',
+  //   '預審狀態',
+  //   '畢業狀態',
+  //   '專業選修',
+  //   '其他選修',
+  //   '網路專業課程',
+  //   '多媒體專業課程',
+  //   '舊制或新制',
+  //   '舊制通識 缺的總學分',
+  //   '當代',
+  //   '文化',
+  //   '歷史',
+  //   '公民',
+  //   '群己',
+  //   '自然',
+  //   '新制通識 缺的總學分',
+  //   '核心',
+  //   '核心社會向度',
+  //   '核心人文向度',
+  //   '校基本素養',
+  //   '跨院',
+  //   '外語的狀態',
+  //   '外語',
+  //   '外語基礎',
+  //   '外語進階(包含二外) ',
+  //   '體育 ',
+  //   '服務學習',
+  //   '藝文護照',
+  //   '導師時間',
+  //   '必修缺課'
+  // ])
+  // for (let i = 0; i < data.length; i++) {
+  //   let data_ = data[i][0]
+  //   let compulse = ''
+  //   console.log(data_)
+  //   if(data_ === undefined) continue
+  //   for (let j = 0; j < data_.compulse.length; j++) {
+  //     compulse += data_.compulse[j]
+  //     compulse += ','
+  //   }
+  //   csvArr.push([
+  //     data_.student_id,
+  //     data_.sname,
+  //     data_.program,
+  //     data_.total_credit,
+  //     data_.en_course,
+  //     data_.submit_status,
+  //     data_.graduate_status,
+  //     data_.pro,
+  //     data_.other,
+  //     data_.net,
+  //     data_.media,
+  //     data_.submit_type === 1 ? '新制' : data[i].submit_type === 0 ? '舊制' : '',
+  //     data_.old_total,
+  //     data_.old_contemp,
+  //     data_.old_culture,
+  //     data_.old_history,
+  //     data_.old_citizen,
+  //     data_.old_group,
+  //     data_.old_science,
+  //     data_.new_total,
+  //     data_.new_core_total,
+  //     data_.new_core_society,
+  //     data_.new_core_humanity,
+  //     data_.new_basic,
+  //     data_.new_cross,
+  //     data_.en_status,
+  //     data_.en_total,
+  //     data_.en_basic,
+  //     data_.en_advanced,
+  //     data_.pe,
+  //     data_.service,
+  //     data_.art,
+  //     data_.mentor,
+  //     compulse
+  //   ])
+  //   //console.log(csvArr[i])
+  // }
+  // dispatch(store_student_csv_data(csvArr))
 }
 
 export const setGradutateState = post_item => dispatch => {
