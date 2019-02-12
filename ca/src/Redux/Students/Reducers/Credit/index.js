54a6774b4ea02377799c22b015b1edaf0ad4ed9a
Chih-Yu <lcd010308@gmail.com>
2019-02-12 21:26:20 +0800

    移除假資料

--- ca/src/Redux/Students/Reducers/Credit/index.js
+++ ca/src/Redux/Students/Reducers/Credit/index.js
@@ -63,78 +63,10 @@ const initialState = {
     file: ''
   },
   creditInfo: {
-    "waive_course": [
-    {
-      "phone": "0912345678",
-      "original_school": "清華大學",
-      "original_department": "資工系",
-      "current_school": "交通大學",
-      "current_department": "資工系",
-      "original_graduation_credit": 128,
-      "apply_year": 106,
-      "apply_semester": 2,
-      "original_course_year": "二",
-      "original_course_semester": "下",
-      "original_course_name": "機率",
-      "original_course_department": "資工系",
-      "original_course_credit": 3,
-      "original_course_score": 80,
-      "current_course_code": "EC4313",
-      "current_course_name": "機率",
-      "current_course_credit": 3,
-      "file": "https://firebasestorage.googleapis.com/v0/b/nctu-csca.appspot.com/o/credit%2F0416000%2F%5Bobject%20File%5D?alt=media&token=a784f690-62a1-4465-8e21-bbe36e9194d1",
-      "status": 0
-    }
-  ],
-  "exempt_course": [
-    {
-      "phone": "0912345678",
-      "apply_year": 106,
-      "apply_semester": 2,
-      "original_course_year": "二",
-      "original_course_semester": "下",
-      "original_course_name": "機率",
-      "original_course_department": "資工系",
-      "original_course_credit": 3,
-      "original_course_score": 80,
-      "current_course_code": "EC4313",
-      "current_course_name": "機率",
-      "current_course_credit": 3,
-      "file": "https://firebasestorage.googleapis.com/v0/b/nctu-csca.appspot.com/o/credit%2F0416000%2F%5Bobject%20File%5D?alt=media&token=a784f690-62a1-4465-8e21-bbe36e9194d1",
-      "status": 1
-    }
-  ],
-  "compulsory_course": [
-    {
-      "apply_year": 107,
-      "apply_semester": 1,
-      "phone": "0912345678",
-      "reason": "系上課很糞",
-      "department": "電機",
-      "teacher": "高榮鴻",
-      "course_code": "UEE2104",
-      "course_name": "機率(英文班)",
-      "original_course_code": "UEE2104",
-      "original_course_name": "機率(英文班)",
-      "credit": 3,
-      "file": "https://firebasestorage.googleapis.com/v0/b/nctu-csca.appspot.com/o/credit%2F0416000%2F%5Bobject%20File%5D?alt=media&token=a784f690-62a1-4465-8e21-bbe36e9194d1",
-      "status": 1
-    }
-  ],
-  "english_course": [
-    {
-      "apply_year": 107,
-      "apply_semester": 1,
-      "phone": "0912345678",
-      "reason": "系上課很糞",
-      "department": "電機",
-      "teacher": "高榮鴻",
-      "course_code": "UEE2104",
-      "course_name": "機率(英文班)",
-      "file": "https://firebasestorage.googleapis.com/v0/b/nctu-csca.appspot.com/o/credit%2F0416000%2F%5Bobject%20File%5D?alt=media&token=a784f690-62a1-4465-8e21-bbe36e9194d1",
-      "status": 2
-    }
-  ]
+    waive_course: [],
+    exempt_course: [],
+    compulsory_course: [],
+    english_course: []
   }
 }
 
