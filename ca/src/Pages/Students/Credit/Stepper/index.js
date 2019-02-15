44f1a17aca0fc8af549e45e3750b69e1627cb7fe
danny <danny021406@gmail.com>
2019-02-16 00:17:14 +0800

    add reg check

--- ca/src/Pages/Students/Credit/Stepper/index.js
+++ ca/src/Pages/Students/Credit/Stepper/index.js
@@ -83,6 +83,7 @@ class HorizontalLinearStepper extends React.Component {
   handleNext () {
     const { stepIndex, selectFormIndex } = this.state
     scrollToComponent(this.Top, { offset: -20, align: 'top', duration: 1000})
+    let foreverCodepattern = new RegExp('[a-zA-Z]+[0-9]+')
 
     if (stepIndex === 0 && selectFormIndex === -1) {
       window.alert('請選擇表單')
@@ -97,6 +98,10 @@ class HorizontalLinearStepper extends React.Component {
           original_course_credit, original_course_score,
           current_course_code, current_course_credit, current_course_name, current_course_type
         } = this.props.waiveCourse
+        if (!current_course_code.match(foreverCodepattern)) {
+          window.alert('請填寫”永久課號“!')
+          return
+        }
         if (
           // 因為syntax問題 所以class這樣寫
           !(file.name && this.props.waiveCourse.class && phone && original_school && original_department &&
@@ -118,6 +123,10 @@ class HorizontalLinearStepper extends React.Component {
           original_course_credit, original_course_score,
           current_course_code, current_course_credit, current_course_name, current_course_type
         } = this.props.exemptCourse
+        if (!current_course_code.match(foreverCodepattern)) {
+          window.alert('請填寫”永久課號“!')
+          return
+        }
         if (
           // 因為syntax問題 所以class這樣寫
           !(file.name && this.props.exemptCourse.class && phone &&
@@ -136,6 +145,10 @@ class HorizontalLinearStepper extends React.Component {
           course_year, course_semester, course_code, course_name,
           original_course_code, original_course_name
         } = this.props.compulsoryCourse
+        if (!course_code.match(foreverCodepattern) || !original_course_code.match(foreverCodepattern) ) {
+          window.alert('請填寫”永久課號“!')
+          return
+        }
         if (
           !(file.name && phone && reason.content && department && teacher &&
             course_year && course_semester && course_code && course_name &&
@@ -147,6 +160,10 @@ class HorizontalLinearStepper extends React.Component {
         this.setState({ file: file })
       } else if (selectFormIndex === 3) {
         const { file, phone, reason, department, teacher, course_code, course_name } = this.props.englishCourse
+        if (!course_code.match(foreverCodepattern)) {
+          window.alert('請填寫”永久課號“!')
+          return
+        }
         if (!(file.name && phone && reason && department && teacher && course_name && course_code)) {
           window.alert('請確實填寫每個欄位!')
           return
