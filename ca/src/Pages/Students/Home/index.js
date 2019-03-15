19d7255a4ded07deb16393c56fbd5bb4b912c4bb
Chih-Yu <lcd010308@gmail.com>
2019-03-15 18:43:43 +0800

    Modify file structure

--- ca/src/Pages/Students/Home/index.js
+++ ca/src/Pages/Students/Home/index.js
@@ -4,9 +4,9 @@ import img0 from '../../../Resources/index01.jpg'
 import img1 from '../../../Resources/index02.png'
 import img2 from '../../../Resources/index03.png'
 import img3 from '../../../Resources/index04.png'
-import './Home.css'
+import './style.css'
 
-class Mentor extends React.Component {
+class Index extends React.Component {
   render () {
     return (
       <div className='home-image-adjust'>
@@ -23,4 +23,4 @@ class Mentor extends React.Component {
   }
 }
 
-export default Mentor
+export default Index
