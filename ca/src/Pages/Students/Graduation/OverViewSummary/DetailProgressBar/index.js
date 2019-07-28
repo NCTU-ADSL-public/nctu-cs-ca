287336ef08dee1bab1e7bb312b03e52629da07ee
Chih-Yu <lcd010308@gmail.com>
2019-07-28 14:14:34 +0800

    Rename variable

--- ca/src/Pages/Students/Graduation/OverViewSummary/DetailProgressBar/index.js
+++ ca/src/Pages/Students/Graduation/OverViewSummary/DetailProgressBar/index.js
@@ -7,16 +7,17 @@ import './style.css'
 const ProgressBar = (props) => (
   <div className='showCourseOverview'>
     <div className='progressBar'>{ props.title }</div>
-    <font size={5} color='#338d68'>{ props.credit }</font>/
-    <div className='progressBar'>{ props.credit_require }</div>
+    <font size={5} color='#338d68'>{ props.complete }</font>/
+    <div className='progressBar'>{ props.require }</div>
     { props.unit }
-    <AnimatedProgress value={props.credit / props.credit_require * 100} /></div>
+    <AnimatedProgress value={props.complete / props.require * 100} />
+  </div>
 )
 
 const NoProgressBar = (props) => (
   <div className='showCourseOverview'>
     <div className='progressBar'>{ props.title }</div>
-    <font size={5} color='#338d68'>{ props.credit }</font>
+    <font size={5} color='#338d68'>{ props.complete }</font>
     <div className='progressBar' />
     { props.unit }
   </div>
@@ -31,30 +32,30 @@ class Index extends React.Component {
         <div className='overview hidden-xs'>
           <div className='overviewCourse col-sm-3 col-md-3'>
             <ProgressBar
-              title={'共同必修'}
-              unit={'學分'}
-              credit={overview.compulsory}
-              credit_require={overview.compulse_require}
+              title='共同必修'
+              unit='學分'
+              complete={overview.compulsory}
+              require={overview.compulse_require}
             />
             <ProgressBar
-              title={'服務學習'}
-              unit={'門'}
-              credit={overview.service}
-              credit_require={overview.service_require}
+              title='服務學習'
+              unit='門'
+              complete={overview.service}
+              require={overview.service_require}
             />
             <ProgressBar
-              title={'英文授課'}
-              unit={'門'}
-              credit={overview.english}
-              credit_require={overview.english_require}
+              title='英文授課'
+              unit='門'
+              complete={overview.english}
+              require={overview.english_require}
             />
           </div>
           <div className='overviewCourse col-sm-3 col-md-3'>
             <ProgressBar
-              title={'藝文賞析'}
-              unit={'門'}
-              credit={overview.art}
-              credit_require={overview.art_require}
+              title='藝文賞析'
+              unit='門'
+              complete={overview.art}
+              require={overview.art_require}
             />
             <ProgressBar
               title={
@@ -62,14 +63,14 @@ class Index extends React.Component {
                   <div className='progressBar2'>通</div>識
                 </div>
               }
-              unit={'學分'}
-              credit={overview.general}
-              credit_require={overview.general_require}
+              unit='學分'
+              complete={overview.general}
+              require={overview.general_require}
             />
             <NoProgressBar
-              title={'抵免研究所課程'}
-              unit={'學分'}
-              credit={overview.graduate}
+              title='抵免研究所課程'
+              unit='學分'
+              complete={overview.graduate}
             />
           </div>
           <div className='overviewCourse col-sm-3 col-md-3'>
@@ -79,15 +80,15 @@ class Index extends React.Component {
                   <div className='progressBar2'>外</div>語
                 </div>
               }
-              unit={'學分'}
-              credit={overview.language}
-              credit_require={overview.language_require}
+              unit='學分'
+              complete={overview.language}
+              require={overview.language_require}
             />
             <ProgressBar
-              title={'專業選修'}
-              unit={'學分'}
-              credit={overview.pro}
-              credit_require={overview.pro_require}
+              title='專業選修'
+              unit='學分'
+              complete={overview.pro}
+              require={overview.pro_require}
             />
           </div>
           <div className='overviewCourse col-sm-3 col-md-3'>
@@ -97,15 +98,15 @@ class Index extends React.Component {
                   <div className='progressBar2'>體</div>育
                 </div>
               }
-              unit={'門'}
-              credit={overview.pe}
-              credit_require={overview.pe_require}
+              unit='門'
+              complete={overview.pe}
+              require={overview.pe_require}
             />
             <ProgressBar
-              title={'其他選修'}
-              unit={'學分'}
-              credit={overview.other}
-              credit_require={overview.other_require}
+              title='其他選修'
+              unit='學分'
+              complete={overview.other}
+              require={overview.other_require}
             />
           </div>
         </div>
