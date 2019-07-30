1c8d5d20b5853569858786a07919714252344384
marmot0814 <s1357andy871207@gmail.com>
2019-07-30 13:46:25 +0800

    check table

--- ca/src/Pages/Assistants/Project/Check/Check.js
+++ ca/src/Pages/Assistants/Project/Check/Check.js
@@ -2,6 +2,11 @@ import React from 'react'
 import { withStyles } from '@material-ui/core/styles'
 import { connect } from 'react-redux'
 
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 
 const styles = theme => ({
   container: {
@@ -25,6 +30,26 @@ class Check extends React.Component {
 
     return (
       <div className={classes.container}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell style={{fontSize: '20px'}}>學號</TableCell>
+              <TableCell style={{fontSize: '20px'}}>姓名</TableCell>
+              <TableCell style={{fontSize: '20px'}}>專題狀態</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          {
+            this.props.Check.checks.map( check => (
+              <TableRow>
+                <TableCell style={{fontSize: '18px'}}>{check.id}</TableCell>
+                <TableCell style={{fontSize: '18px'}}>{check.name}</TableCell>
+                <TableCell style={{fontSize: '18px'}}>{check.first_second}</TableCell>
+              </TableRow>
+            ))
+          }
+          </TableBody>
+        </Table>
       {
         this.props.Check.checks.map( check => (
           <div>
