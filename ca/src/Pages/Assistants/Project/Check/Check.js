6136867122922d60a396165e812e0ae9c9e3d526
marmot0814 <s1357andy871207@gmail.com>
2019-08-01 08:41:17 +0800

    assistant project check  test page layout

--- ca/src/Pages/Assistants/Project/Check/Check.js
+++ ca/src/Pages/Assistants/Project/Check/Check.js
@@ -9,6 +9,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import DoneIcon from '@material-ui/icons/Done';
 import ClearIcon from '@material-ui/icons/Clear';
+import IconButton from '@material-ui/core/IconButton';
 
 const styles = theme => ({
   container: {
@@ -16,6 +17,8 @@ const styles = theme => ({
     margin: '0 auto'
   }
 })
+
+const FIRST_SECOND_CN = ['', '專題(一)', '專題(二)', '基礎程式設計通過待確認']
 
 class Check extends React.Component {
 
@@ -59,26 +62,34 @@ class Check extends React.Component {
         <Table>
           <TableHead>
             <TableRow style={{display: 'flex', justifyContent: 'center'}}>
-              <TableCell style={{fontSize: '20px', flex: 0.05, paddingTop: '11px'}} />
-              <TableCell style={{fontSize: '20px', flex: 0.05, paddingTop: '11px'}} />
-              <TableCell style={{fontSize: '20px', flex: 0.2, paddingTop: '11px'}}>學號</TableCell>
-              <TableCell style={{fontSize: '20px', flex: 0.2, paddingTop: '11px'}}>姓名</TableCell>
-              <TableCell style={{fontSize: '20px', flex: 0.4, paddingTop: '11px'}}>專題狀態</TableCell>
+              <TableCell style={{flex: 0.025, padding: '0px'}} >
+                <IconButton style={{fontSize: '18px'}} disabled/>
+              </TableCell>
+              <TableCell style={{flex: 0.025, padding: '0px'}} >
+                <IconButton style={{fontSize: '18px'}} disabled/>
+              </TableCell>
+              <TableCell style={{fontSize: '25px', flex: 0.175, paddingTop: '11px', paddingLeft: '20px'}}>學號</TableCell>
+              <TableCell style={{fontSize: '25px', flex: 0.175, paddingTop: '11px', paddingLeft: '0px'}}>姓名</TableCell>
+              <TableCell style={{fontSize: '25px', flex: 0.6, paddingTop: '11px', paddingLeft: '0px'}}>專題狀態</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
           {
-            this.props.Check.checks.map( check => (
-              <TableRow hover style={{display: 'flex', justifyContent: 'center'}}>
-                <TableCell style={{fontSize: '18px', flex: 0.05, paddingTop: '11px'}}>
-                  <DoneIcon />
+            this.props.Check.checks.map( (check, idx) => (
+              <TableRow hover style={{display: 'flex', justifyContent: 'center'}} key={idx}>
+                <TableCell style={{flex: 0.025, padding: '0px'}}>
+                  <IconButton style={{color: 'green', fontSize: '18px'}}>
+                    <DoneIcon />
+                  </IconButton>
                 </TableCell>
-                <TableCell style={{fontSize: '18px', flex: 0.05, paddingTop: '11px'}}>
-                  <ClearIcon />
+                <TableCell style={{flex: 0.025, padding: '0px'}}>
+                  <IconButton style={{color: 'red', fontSize: '18px'}}>
+                    <ClearIcon />
+                  </IconButton>
                 </TableCell>
-                <TableCell style={{fontSize: '18px', flex: 0.2, paddingTop: '11px'}}>{this.hightlight(check.id, this.props.Check.input)}</TableCell>
-                <TableCell style={{fontSize: '18px', flex: 0.2, paddingTop: '11px'}}>{this.hightlight(check.name, this.props.Check.input)}</TableCell>
-                <TableCell style={{fontSize: '18px', flex: 0.4, paddingTop: '11px'}}>{this.hightlight(check.first_second, this.props.Check.input)}</TableCell>
+                <TableCell style={{fontSize: '18px', flex: 0.175, paddingTop: '11px', paddingLeft: '20px'}}>{this.hightlight(check.id, this.props.Check.input)}</TableCell>
+                <TableCell style={{fontSize: '18px', flex: 0.175, paddingTop: '11px', paddingLeft: '0px'}}>{this.hightlight(check.name, this.props.Check.input)}</TableCell>
+                <TableCell style={{fontSize: '18px', flex: 0.6, paddingTop: '11px', color: parseInt(check.first_second)=== 3 ? 'red' : '', paddingLeft: '0px'}}>{this.hightlight(FIRST_SECOND_CN[check.first_second], this.props.Check.input)}</TableCell>
               </TableRow>
             ))
           }
