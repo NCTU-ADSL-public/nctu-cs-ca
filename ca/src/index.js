edc9757c86dc864467e0ada79f664405dbdf2a3c
marmot0814 <s1357andy871207@gmail.com>
2019-08-19 20:43:37 +0800

    check env

--- ca/src/index.js
+++ ca/src/index.js
@@ -7,7 +7,7 @@ import {createStore, applyMiddleware} from 'redux'
 import Reducers from './Redux'
 import {Provider} from 'react-redux'
 import thunk from 'redux-thunk'
-
+console.log(process.env)
 let store = createStore(Reducers, applyMiddleware(thunk))
 
 ReactDOM.render(<Provider store={store}><Router /></Provider>, document.getElementById('root'))
