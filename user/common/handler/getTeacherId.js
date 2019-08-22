927d1d15f5c31e8a5c57333c591a5f584a962d10
JoyingKuo <joying62757@gmail.com>
2019-08-22 12:25:21 +0800

    add test account for assistant

--- user/common/handler/getTeacherId.js
+++ user/common/handler/getTeacherId.js
@@ -4,12 +4,23 @@ var getTeacherId = {};
 getTeacherId.teacherId = function(req, res, next){
     
     if(req.session.profile){
-        res.locals.teacherId = utils.getPersonId(JSON.parse(req.session.profile));;
+        //res.locals.teacherId = utils.getPersonId(JSON.parse(req.session.profile));;
         if(res.locals.teacherId == '0316201' || res.locals.teacherId == '0312512'||res.locals.teacherId == '0416014' || res.locals.teacherId == '0416008'||res.locals.teacherId == '0416004' || res.locals.teacherId == '0416081' || res.locals.teacherId == '0516003' || res.locals.teacherId == '0516205'||res.locals.teacherId =='0616005')
-        //res.locals.teacherId = 'T9229';
-        //console.log("getting");
-        //res.locals.teacherId = 'T8058';
-         res.locals.teacherId = 'T0525';
+            res.locals.teacherId = 'T0525';
+
+        let checkPage = req.originalUrl;
+        let checkIndex = checkPage.indexOf("/", 1);
+        checkPage = checkPage.substring(1, checkIndex);
+
+        if(checkPage === "teachers"){
+
+           res.locals.teacherId = utils.getPersonId(JSON.parse(req.session.profile));
+
+            if (res.locals.teacherId[0] == 'E') {
+                res.locals.teacherId = 'T9229';
+            }
+        }
+
         next();
     }
     else
